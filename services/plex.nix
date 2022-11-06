c17d1ad59255e8e613db18e2bda97628e3e34fb6
Krutonium <PFCKrutonium@gmail.com>
2022-11-06 01:38:08 -0400

    Plex should not always be enabled :P

--- services/plex.nix
+++ services/plex.nix
@@ -1,8 +1,15 @@
 { config, pkgs, lib, ... }:
+with lib;
+with builtins;
+let
+  cfg = config.sys.services;
+in
 {
-  networking.firewall.allowedTCPPorts = [ 32400 ];
-  services.plex = {
-    enable = true;
-    openFirewall = true;
+  config = mkIf (cfg.plex == true) {
+    networking.firewall.allowedTCPPorts = [ 32400 ];
+    services.plex = {
+      enable = true;
+      openFirewall = true;
+    };
   };
 }
