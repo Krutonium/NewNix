5092eee8810db8ad6252b04dce1b19d75ec0121d
Krutonium <PFCKrutonium@gmail.com>
2022-11-18 14:28:18 -0500

    Remove hard coded IPv6 to attempt to get a "working" one.

--- devices/uWebServer.nix
+++ devices/uWebServer.nix
@@ -7,10 +7,10 @@ in
   networking.firewall.allowedTCPPorts = [ 25565 ];
   boot.kernelPackages = kernel;
   networking.hostName = Hostname;
-  networking.interfaces."enp3s0".ipv6.addresses = [{ address = "2607:fea8:7a40:f10::6969"; prefixLength = 64; }];
+  #networking.interfaces."enp3s0".ipv6.addresses = [{ address = "2607:fea8:7a40:f10::6969"; prefixLength = 64; }];
   networking.interfaces."enp3s0".ipv4.addresses = [{ address = "192.168.0.10"; prefixLength = 24; }];
   networking.defaultGateway = { address = "192.168.0.1"; interface = "enp3s0"; };
-  networking.defaultGateway6 = { address = "fe80::1"; interface = "enp3s0"; };
+  #networking.defaultGateway6 = { address = "fe80::1"; interface = "enp3s0"; };
   imports = [ ./uWebServer-hw.nix ];
   sys = {
     boot = {
