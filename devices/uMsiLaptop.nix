919408161b82ee5885708fd59889b0a140d4e59a
Krutonium <PFCKrutonium@gmail.com>
2022-11-20 22:39:17 -0500

    nVidia Stuff

--- devices/uMsiLaptop.nix
+++ devices/uMsiLaptop.nix
@@ -2,6 +2,13 @@
 let
   kernel = pkgs.linuxPackages_zen;
   Hostname = "uMsiLaptop";
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
 in
 {
   boot.kernelPackages = kernel;
@@ -33,10 +40,22 @@ in
       steam = true;
     };
   };
+  boot.kernelModules = [ "mem_sleep_default=deep" ];
   specialisation."nVidia".configuration = {
     system.nixos.tags = [ "with-nvidia" ];
     services.xserver.videoDrivers = [ "nvidia" ];
     hardware.opengl.enable = true;
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+    environment.systemPackages = [ nvidia-offload ];
+    services.xserver.videoDrivers = [ "modesetting" ];
+    hardware.nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
+      powermanagement.enable = true;
+      prime = {
+        offload.enable = true;
+        nvidiaBusId = "PCI:1:0:0";
+        intelBusId = "PCI:0:2:0";
+      };
+    };
   };
 }
