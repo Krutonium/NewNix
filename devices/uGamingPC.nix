eb3eeab6306f57963776c0fa300917f8a45c09ed
Krutonium <PFCKrutonium@gmail.com>
2022-11-23 01:56:38 -0500

    Forgot a ;

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -9,8 +9,7 @@ in
   imports = [ ./uGamingPC-hw.nix ];
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.nvidia_x11_legacy470
-    hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.nvidia_x11_legacy470;
   boot.initrd.availableKernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
   sys = {
     boot = {
