552ea6994d6a4d45d660c51596acdea964c54f88
Krutonium <PFCKrutonium@gmail.com>
2022-11-22 20:15:05 -0500

    Overlays to fix Electron

---
+++ overlays/overlay.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, lib, ... }:
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      discord =
+        final.symlinkJoin {
+          name = "discord";
+          paths = [ prev.discord ];
+          nativeBuildInputs = [ final.makeWrapper ];
+
+          postBuild = ''
+            wrapProgram $out/bin/discord --add-flags "--use-gl=desktop"
+            wrapProgram $out/bin/Discord --add-flags "--use-gl=desktop"
+          '';
+
+          passthru.unwrapped = prev.discord;
+
+          meta = {
+            inherit (prev.discord.meta) homepage description longDescription maintainers;
+            mainProgram = "discord";
+          };
+        };
+    })
+  ]
+    [
+      (final: prev: {
+        element-desktop =
+          final.symlinkJoin {
+            name = "element-desktop";
+            paths = [ prev.element-desktop ];
+            nativeBuildInputs = [ final.makeWrapper ];
+
+            postBuild = ''
+              wrapProgram $out/bin/element-desktop --add-flags "--use-gl=desktop"
+            '';
+            passthru.unwrapped = prev.element-desktop;
+            meta = {
+              inherit (prev.discord.meta) homepage description longDescription maintainers;
+              mainProgram = "element-desktop";
+            };
+          };
+      })
+    ];
+}
