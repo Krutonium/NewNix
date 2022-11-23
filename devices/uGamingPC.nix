06dd45a131371f0ffeae7d66de77116fb5a9a904
Krutonium <PFCKrutonium@gmail.com>
2022-11-23 01:59:18 -0500

    Fixed

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -9,7 +9,7 @@ in
   imports = [ ./uGamingPC-hw.nix ];
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.nvidia_x11_legacy470;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy470;
   boot.initrd.availableKernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
   sys = {
     boot = {
