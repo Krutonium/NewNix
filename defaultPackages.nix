95fa983528bb5d39b94ded5df133c601a24d92d8
Krutonium <PFCKrutonium@gmail.com>
2022-11-26 04:48:48 -0500

    Bring back my Fonts

--- defaultPackages.nix
+++ defaultPackages.nix
@@ -1,5 +1,23 @@
 { config, pkgs, pkgs-unstable, ... }:
+let
+  fontFiles = pkgs.fetchzip {
+    url = "https://gitea.krutonium.ca/Krutonium/NixOS_Files/raw/branch/master/Fonts.zip";
+    sha256 = "sha256-44KDjOw0/pGWIeD3o2V2WJJl7rScq8OftmfC549FhGA=";
+    stripRoot = false;
+  };
+  fonts = pkgs.stdenv.mkDerivation {
+    name = "Additional Fonts";
+    src = fontFiles;
+    buildInputs = [ ];
+    buildCommand =
+      ''
+        mkdir -p $out/share/fonts
+        cp -R $src $out/share/fonts/opentype/
+      '';
+  };
+in
 {
+  fonts.fonts = [ fonts ];
   environment.systemPackages = [
     pkgs.nano #Editor
     pkgs.git
