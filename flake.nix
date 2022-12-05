b53de1ee6c82bdf0cfc2ac6d356db0f650aac589
Krutonium <PFCKrutonium@gmail.com>
2022-12-05 02:15:18 -0500

    Realized I don't need Master

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11"; # NixOS release channel
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # NixOS unstable channel
-    nixpkgs-master.url = "github:NixOS/nixpkgs/master"; #NixOS Master
     nixos-hardware.url = "github:NixOS/nixos-hardware/master"; # NixOS hardware channel
     home-manager.url = "github:nix-community/home-manager/release-22.11"; # Home Manager release channel
     update = {
