49be4181e5e52ae8aee9384a74b59ede7ae0c702
Krutonium <PFCKrutonium@gmail.com>
2022-12-06 03:48:30 -0500

    Switch to Zsh

--- user/krutonium-hm-extras/terminal.nix
+++ user/krutonium-hm-extras/terminal.nix
@@ -10,7 +10,7 @@
   };
   programs.starship = {
     enable = true;
-    enableFishIntegration = true;
+    enableZshIntegration = true;
     settings = {
       format = builtins.concatStringsSep "" [
         "$username"
--- user/krutonium.nix
+++ user/krutonium.nix
@@ -10,13 +10,14 @@ in
       uid = 1002;
       home = "/home/krutonium";
       isNormalUser = true;
-      shell = pkgs.fish;
+      shell = pkgs.zsh;
       extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" "deluge" ];
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBGydZMghVpYF+glHje55hN0/00i9nOEA+OP4A/eneXp" ];
       hashedPassword = "$6$l5HeZlsZILfJPHoJ$bE95YsS6Xu1kTj9RgPKpd4JblUsoA35UmCrqFdr5N71HNa3T3SA3Nw.RxT4ifqF239DzYECcyZQZQGLCtFb8W/";
     };
-    programs.fish.enable = true;
-    programs.fish.useBabelfish = true;
+    #programs.fish.enable = true;
+    #programs.fish.useBabelfish = true;
+    programs.zsh.enable = true;
     home-manager.users.krutonium =
       if (cfg.home-manager == true) then
         import ./krutonium-hm.nix
