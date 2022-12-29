c4ab65e57f272552c593b127b11d66049ec799a4
Krutonium <PFCKrutonium@gmail.com>
2022-12-28 22:34:21 -0500

    deploy

--- virtualization/virtd.nix
+++ virtualization/virtd.nix
@@ -7,6 +7,8 @@ in
 {
   config = mkIf (cfg.server == "virtd") {
     virtualisation.libvirtd.enable = true;
+    virtualisation.libvirtd.qemu.swtpm.enable = true;
+    virtualisation.libvirtd.qemu.ovmf.enable = true;
     programs.dconf.enable = true;
     environment.systemPackages = with pkgs; [ virt-manager ];
     users.users.krutonium.extraGroups = [ "libvirtd" ];
