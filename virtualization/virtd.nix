3d32e5f5aadc1cb98c49717056566f54559d4e00
Krutonium <PFCKrutonium@gmail.com>
2023-01-01 23:26:46 -0500

    deploy

--- virtualization/virtd.nix
+++ virtualization/virtd.nix
@@ -15,7 +15,7 @@ in
       qemu = {
         package = pkgs.qemu_kvm;
         ovmf.enable = true;
-        ovmf.package = pkgs.OVMFFull;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
         swtpm.enable = true;
         runAsRoot = false;
       };
@@ -24,12 +24,6 @@ in
     environment.etc = {
       "ovmf/edk2-x86_64-secure-code.fd" = {
         source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
-      };
-
-      "ovmf/edk2-i386-vars.fd" = {
-        source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
-        mode = "0644";
-        user = "libvirtd";
       };
     };
     environment.systemPackages = [ pkgs.swtpm pkgs.virt-manager ];
