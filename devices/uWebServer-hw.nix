d1218c1d169936cfc7409613a97204c05f36610e
Krutonium <PFCKrutonium@gmail.com>
2023-01-06 01:53:18 -0500

    deploy

--- devices/uWebServer-hw.nix
+++ devices/uWebServer-hw.nix
@@ -167,7 +167,7 @@
     {
       device = "/dev/disk/by-uuid/a018b12f-6567-4edb-8026-be9292738b4d";
       fsType = "btrfs";
-      option = [ "subvol=gameserver" ];
+      options = [ "subvol=gameserver" ];
     };
 
 
