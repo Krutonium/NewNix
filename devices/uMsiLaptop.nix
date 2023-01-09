c6bf8437ec68c69459d95f0b2b94c114cf681017
Krutonium <PFCKrutonium@gmail.com>
2023-01-09 01:11:57 -0500

    :(

--- devices/uMsiLaptop.nix
+++ devices/uMsiLaptop.nix
@@ -61,7 +61,6 @@ in
     services.xserver.videoDrivers = [ "nvidia" "modesetting" ];
     hardware.opengl.enable = true;
     environment.systemPackages = [ nvidia-offload ];
-    sys.desktop.wayland = false;
     hardware.nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.stable;
       modesetting.enable = true;
