dce624b6933906e03965b6724a3e0f1935b7b919
Krutonium <PFCKrutonium@gmail.com>
2023-01-17 06:47:34 -0500

    *sadness*

--- user/krutonium-hm-extras/hyprland.nix
+++ user/krutonium-hm-extras/hyprland.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 {
-  if (networking.hostName == "uGamingPC") {
+  p = mkIf (config.networking.hostName == "uGamingPC") {
     home.file = {
       ".config/hypr/displays.conf".text = ''
         monitor=HDMI-A-1, 1920x1080@144, 3840x0, 1
