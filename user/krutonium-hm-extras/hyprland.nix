9566c7d72c7b2f2116e0d9a7a22e4cfb7ce35d5c
Krutonium <PFCKrutonium@gmail.com>
2023-01-17 06:37:27 -0500

    text

--- user/krutonium-hm-extras/hyprland.nix
+++ user/krutonium-hm-extras/hyprland.nix
@@ -2,7 +2,7 @@
 {
   if (networking.hostName == "uGamingPC") {
     home.file = {
-      ".config/hypr/displays.conf" = ''
+      ".config/hypr/displays.conf".text = ''
         monitor=HDMI-A-1, 1920x1080@144, 3840x0, 1
         monitor=DP-1,     1920x1080@165, 1920x0, 1
         monitor=DP-2,     1920x1080@165, 0x0,    1
