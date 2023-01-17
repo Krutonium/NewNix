c5554ba5475ed6ca100307b6254a4afaab902bdd
Krutonium <PFCKrutonium@gmail.com>
2023-01-17 06:36:18 -0500

    Not sure why If isn't working

--- user/krutonium-hm-extras/hyprland.nix
+++ user/krutonium-hm-extras/hyprland.nix
@@ -1,12 +1,13 @@
-{ config, pkgs, ... }
+{ config, pkgs, lib, ... }:
 {
-if (networking.hostName == "uGamingPC") {
-home.file = {
-".config/hypr/displays.conf" = ''
+  if (networking.hostName == "uGamingPC") {
+    home.file = {
+      ".config/hypr/displays.conf" = ''
         monitor=HDMI-A-1, 1920x1080@144, 3840x0, 1
         monitor=DP-1,     1920x1080@165, 1920x0, 1
-        monitor=DP-1,     1920x1080@165, 0x0,    1
+        monitor=DP-2,     1920x1080@165, 0x0,    1
       '';
-};
-};
+    };
+  };
 }
+
