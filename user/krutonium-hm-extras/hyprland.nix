0fa060cb354d49a656fb05e7a1b6aff0a264f42a
Krutonium <PFCKrutonium@gmail.com>
2023-01-18 01:33:18 -0500

    Laptop

--- user/krutonium-hm-extras/hyprland.nix
+++ user/krutonium-hm-extras/hyprland.nix
@@ -1,13 +1,12 @@
 { config, pkgs, lib, ... }:
 {
-  config = lib.mkIf (config.networking.hostName == "uGamingPC") {
-    home.file = {
-      ".config/hypr/displays.conf".text = ''
-        monitor=HDMI-A-1, 1920x1080@144, 3840x0, 1
-        monitor=DP-1,     1920x1080@165, 1920x0, 1
-        monitor=DP-2,     1920x1080@165, 0x0,    1
-      '';
-    };
+  home.file = {
+    ".config/hypr/displays.conf".text = ''
+      monitor=HDMI-A-1, 1920x1080@144, 3840x0, 1
+      monitor=DP-1,     1920x1080@165, 1920x0, 1
+      monitor=DP-2,     1920x1080@165, 0x0,    1
+      monitor=eDP-1,    1920x1080@60,  0x0,     1
+    '';
   };
 }
 
