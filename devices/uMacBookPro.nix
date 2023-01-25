fdcb542e885fe6aeb56e6a9b6d1748ae490c1822
Krutonium <PFCKrutonium@gmail.com>
2023-01-24 21:46:10 -0500

    Fix typo

--- devices/uMacBookPro.nix
+++ devices/uMacBookPro.nix
@@ -9,7 +9,7 @@ in
   hardware.enableAllFirmware = true;
   boot.kernelPackages = kernel;
   boot.initrd.availableKernelModules = [ "nouveau" ]; #Fix not grabbing the display before login manager
-  mports = [ ./uMacBookPro-hw.nix ];
+  imports = [ ./uMacBookPro-hw.nix ];
   sys = {
     boot = {
       bootloader = "uefi";
