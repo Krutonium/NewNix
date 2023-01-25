abfb6d473f32c384e069e07b8196cdd66ee3d315
Krutonium <PFCKrutonium@gmail.com>
2023-01-25 05:20:47 -0500

    AllowBroken?

--- devices/uMacBookPro.nix
+++ devices/uMacBookPro.nix
@@ -13,7 +13,7 @@ in
     package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
   };
   services.xserver.videoDrivers = [ "nvidia" ];
-  allowBroken = true;
+  config.allowBroken = true;
 
   imports = [ ./uMacBookPro-hw.nix ];
   sys = {
