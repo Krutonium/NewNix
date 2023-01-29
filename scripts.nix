7811059a2ef134503334e1c861da8995fed9e184
Krutonium <PFCKrutonium@gmail.com>
2023-01-29 10:33:46 -0500

    Add script

--- scripts.nix
+++ scripts.nix
@@ -5,8 +5,12 @@ let
     ssh $@
     until !!; do sleep 5 ; done
   '';
+  updateindex = pkgs.writeShellScriptBin "updateindex" ''
+    mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
+    wget -q -N https://github.com/Mic92/nix-index-database/releases/latest/download/index-x86_64-linux -O files
+  '';
 
 in
 {
-  environment.systemPackages = [ sshr ];
+  environment.systemPackages = [ sshr updateindex ];
 }
