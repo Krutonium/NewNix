511b1b1953dd5d3ca27fa0ff0e4a2b8e47973d2c
Krutonium <PFCKrutonium@gmail.com>
2023-02-01 20:18:50 -0500

    Install Runescape

--- user/krutonium-hm-extras/packages.nix
+++ user/krutonium-hm-extras/packages.nix
@@ -167,6 +167,7 @@ in
       # Gaming
       # Steam is already installed at the system level because it has special requirements
       pkgs-master.openrct2
+      pkgs.runescape
       pkgs.jstest-gtk
       pkgs-unstable.prismlauncher
       pkgs.protonup
