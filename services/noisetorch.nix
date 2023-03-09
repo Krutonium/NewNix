3e01ce032e79b70b81e396012ade984fcdbf8a6b
Krutonium <PFCKrutonium@gmail.com>
2023-03-09 17:35:44 -0500

    deploy

--- services/noisetorch.nix
+++ services/noisetorch.nix
@@ -8,13 +8,13 @@ in
   config = mkIf (cfg.noisetorch == true) {
     #Run the service and also configure the correct device.
     systemd.services.noisetorch = {
-        serviceConfig.Type = "oneshot";
-        serviceConfig.User = "krutonium";
-        path = with pkgs; [ pkgs.noisetorch ];
-        script = ''
-            noisetorch -s ${cfg.noisetorchDevice} -i -o
-        '';
-        enable = true;
-      };
+      serviceConfig.Type = "oneshot";
+      serviceConfig.User = "krutonium";
+      path = with pkgs; [ pkgs.noisetorch ];
+      script = ''
+        noisetorch -s ${cfg.noisetorchDevice} -i -o
+      '';
+      enable = true;
+    };
   };
 }
