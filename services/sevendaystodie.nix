2c6b63f26437074f54a052eb4055d1948220499b
Krutonium <PFCKrutonium@gmail.com>
2023-03-11 19:26:54 -0500

    deploy

--- services/sevendaystodie.nix
+++ services/sevendaystodie.nix
@@ -8,17 +8,17 @@ in
 {
   config = mkIf (cfg.sevendaystodie == true) {
     networking.firewall.allowedTCPPorts = [ 26900 ];
-    networking.firewall.allowedUDPPortRanges = [ { from = 26900; to = 26903; } ];
-    systemd = {          
+    networking.firewall.allowedUDPPortRanges = [{ from = 26900; to = 26903; }];
+    systemd = {
       services."7daystodie" = {
         serviceConfig.Type = "oneshot";
         serviceConfig.User = "krutonium";
         path = with pkgs; [ pkgs.steam-run pkgs.steamcmd ];
         script = ''
-	  mkdir -p ${gamePath}
-          cd ${gamePath}
-          steamcmd +force_install_dir ${gamePath} +login anonymous +app_update 294420 +quit
-          steam-run ./startserver.sh -configfile=serverconfig-7dtd.xml
+          	  mkdir -p ${gamePath}
+                    cd ${gamePath}
+                    steamcmd +force_install_dir ${gamePath} +login anonymous +app_update 294420 +quit
+                    steam-run ./startserver.sh -configfile=serverconfig-7dtd.xml
         '';
         enable = true;
       };
