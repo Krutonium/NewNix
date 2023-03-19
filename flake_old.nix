dd8075c459d0e2021a0e98416eddff8be655fb71
Krutonium <PFCKrutonium@gmail.com>
2023-03-19 04:13:24 -0400

    deploy

--- flake_old.nix
+++ flake_old.nix
@@ -24,7 +24,7 @@
         {
           nix.registry.nixos.flake = inputs.self;
           environment.etc."nix/inputs/nixpkgs".source = nixpkgs.outPath;
-          nix.nixPath = ["nixpkgs=/etc/nix/inputs/nixpkgs"];
+          nix.nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" ];
         }
         home-manager.nixosModules.home-manager
         {
