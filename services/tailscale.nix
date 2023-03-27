c423c48072360bae70468f0f31523a897413a90c
Krutonium <PFCKrutonium@gmail.com>
2023-03-27 12:37:47 -0400

    Automatically use uWebServer as Exit Node

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -12,5 +12,19 @@ in
       trustedInterfaces = [ "tailscale0" ];
       allowedUDPPorts = [ config.services.tailscale.port ];
     };
+    systemd.services.tailscaleConnect = {
+      description = "Connects to TailScale and Routes Traffic";
+      serviceConfig = {
+        Type = "simple";
+        User = "root";
+      };
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      path = [ pkgs.tailscale ];
+      script = ''
+        tailscale up --exit-node=uwebserver
+      '';
+      enable = true;
+    };
   };
 }
