802591013e156dd206f2fa83cb1f62329c397fb7
Krutonium <PFCKrutonium@gmail.com>
2023-04-02 13:23:01 -0400

    ???

--- user/krutonium-hm-extras/packages.nix
+++ user/krutonium-hm-extras/packages.nix
@@ -163,6 +163,7 @@ in
 
   programs.obs-studio = {
     enable = true;
+    package = pkgs.teleport.obs-studio;
     plugins = with pkgs.teleport.obs-studio-plugins; [
       obs-backgroundremoval
       obs-multi-rtmp
