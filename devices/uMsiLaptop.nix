103525e56a4130e935dd203648e5150ab2df3adc
Krutonium <PFCKrutonium@gmail.com>
2023-04-13 22:58:48 -0400

    Cleanup nVidia

--- devices/uMsiLaptop.nix
+++ devices/uMsiLaptop.nix
@@ -4,7 +4,6 @@ let
   Hostname = "uMsiLaptop";
   nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
     export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
     export __GLX_VENDOR_LIBRARY_NAME=nvidia
     export __VK_LAYER_NV_optimus=NVIDIA_only
     exec -a "$0" "$@"
