1d29ea7b13094d8c85d58e2e2a591ffb5a100117
Krutonium <PFCKrutonium@gmail.com>
2023-04-28 21:27:15 -0400

    deploy

--- devices/uWebServer.nix
+++ devices/uWebServer.nix
@@ -12,7 +12,7 @@ in
     kernel.sysctl = {
       "net.ipv4.conf.all.forwarding" = true;
       "net.ipv6.conf.all.forwarding" = true;
-      "net.ipv6.conf.all.accept_ra" = 0;
+      "net.ipv6.conf.all.accept_ra" = 1;
       "net.ipv6.conf.all.autoconf" = 0;
       "net.ipv6.conf.all.use_tempaddr" = 0;
     };
