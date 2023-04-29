a38f465dd020e39a2141dfed1235f3d4bc31e38e
Krutonium <PFCKrutonium@gmail.com>
2023-04-28 20:02:33 -0400

    Remove dhcp6 for now

--- devices/uWebServer.nix
+++ devices/uWebServer.nix
@@ -54,18 +54,6 @@ in
     defaultGateway6 = { address = "fe80::1"; interface = Internet_In; };
     tempAddresses = "disabled";
   };
-
-  #services.dhcpd6 = {
-  #  enable = true;
-  #  interfaces = [ "enp2s0f0" ];
-  #  extraConfig = ''
-  #    option dhcp6.name-servers = "2001:4860:4860::8888, 2001:4860:4860::8844";
-  #    subnet6 2607:fea8:7a5f:2a00::/64 {
-  #      range6 2607:fea8:7a5f:2a00::2 2607:fea8:7a5f:2a00::254;
-  #      interface enp2s0f0;
-  #    }
-  #  '';
-  #};
   services.dhcpd4 = {
     enable = true;
     interfaces = [ "bridge" ];
