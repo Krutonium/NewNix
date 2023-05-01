b509646c81e04936c579e8241e3dceb424a10d5c
Krutonium <PFCKrutonium@gmail.com>
2023-05-01 06:00:13 -0400

    Deploy

--- devices/uWebServer-hw.nix
+++ devices/uWebServer-hw.nix
@@ -199,12 +199,12 @@
       fsType = "btrfs";
       options = [ "subvol=homeAssistant" ];
     };
-  fileSystems."/tmp" =
-    {
-      device = "/dev/disk/by-uuid/a018b12f-6567-4edb-8026-be9292738b4d";
-      fsType = "btrfs";
-      options = [ "subvol=tmp" ];
-    };
+  #fileSystems."/tmp" =
+  #  {
+  #    device = "/dev/disk/by-uuid/a018b12f-6567-4edb-8026-be9292738b4d";
+  #    fsType = "btrfs";
+  #    options = [ "subvol=tmp" ];
+  #  };
   swapDevices = [ ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
