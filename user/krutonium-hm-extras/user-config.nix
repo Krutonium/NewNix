ae3ef20e6ac7a5499d5e4a3611a516d051694550
Krutonium <PFCKrutonium@gmail.com>
2023-06-01 03:57:25 -0400

    forgot profile

--- user/krutonium-hm-extras/user-config.nix
+++ user/krutonium-hm-extras/user-config.nix
@@ -145,13 +145,13 @@
         bat $argv;
       end
     '';
-    ".config/burn-my-windows/1685604310611376.conf".text = ''
+    ".config/burn-my-windows/profiles/1685604310611376.conf".text = ''
        [burn-my-windows-profile]
        profile-animation-type=1
        fire-enable-effect=false
        hexagon-enable-effect=true
     '';
-    ".config/burn-my-windows/1685604429464762.conf".text = '' 
+    ".config/burn-my-windows/profiles/1685604429464762.conf".text = '' 
       [burn-my-windows-profile]
       profile-window-type=0
       profile-animation-type=2
