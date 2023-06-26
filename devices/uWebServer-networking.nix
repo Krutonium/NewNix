614b6a231737fc661937a2e8f8be2bb19ccfcc50
Krutonium <PFCKrutonium@gmail.com>
2023-06-26 01:35:32 -0400

    Tweaks and Lock Update

--- devices/uWebServer-networking.nix
+++ devices/uWebServer-networking.nix
@@ -8,7 +8,7 @@
     "net.ipv6.conf.all.use_tempaddr" = 0;
   };
   systemd.network.enable = true;
-  services.resolved.enable = true;
+  #services.resolved.enable = true;
   networking = {
     # We're not using NetworkManager, as it apparently kinda just dies when given access to the raw internet.
     networkmanager.enable = lib.mkForce false;
