6b28a06055605b9eecf3e69c7b36feca4ce94395
Krutonium <PFCKrutonium@gmail.com>
2023-08-09 20:36:49 -0400

    Updates

--- services/torrent.nix
+++ services/torrent.nix
@@ -6,8 +6,8 @@ let
 in
 {
   config = mkIf (cfg.torrent == true) {
-    networking.firewall.allowedTCPPorts = [ 50023 ];
-    networking.firewall.allowedUDPPorts = [ 50023 ];
+    networking.firewall.allowedTCPPorts = [ 50023 50050 ];
+    networking.firewall.allowedUDPPorts = [ 50023 50050 ];
     networking.firewall.interfaces."bridge".allowedTCPPorts = [ 8112 58846 ];
     services.deluge = {
       package = pkgs.deluge;
@@ -17,8 +17,8 @@ in
       declarative = true;
       authFile = "/persist/deluge.auth";
       config = {
-        max_upload_speed = "100.0";
-        share_ratio_limit = "120.0";
+        max_upload_speed = "1000.0";
+        share_ratio_limit = "500";
         dont_count_slow_torrents = true;
         pre_allocate_storage = true;
         listen_ports = [ 50023 50023 ];
@@ -31,6 +31,13 @@ in
         max_connections_global = 600;
         max_upload_slots_global = -1;
         allow_remote = true;
+        max_active_seeding = "500";
+        max_active_downloading = "500";
+        max_active_limit = "500";
+        listen_interface = "enp4s0";
+        outgoing_interface = "enp4s0";
+        outgoing_ports = [50024 50050];
+        random_outgoing_ports = false;
       };
       web = {
         enable = true;
