a0508b42eb2c21d496ed1f287f37eec9946b48b0
Krutonium <PFCKrutonium@gmail.com>
2023-08-11 17:00:56 -0400

    Fixed xrandr line

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -39,7 +39,7 @@ in
       '';
     logFile = null;
     displayManager.setupCommands = ''
-      xrandr --output HDMI-O --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal
+      xrandr --output HDMI-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal
     '';
     };
   hardware.opengl.enable = true;
