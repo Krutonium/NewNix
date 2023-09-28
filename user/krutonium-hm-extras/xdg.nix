035749c0c5eae6eca4931c4a152ff219c1ea8f6e
Krutonium <PFCKrutonium@gmail.com>
2023-09-27 21:00:42 -0400

    Gul Ducat

--- user/krutonium-hm-extras/xdg.nix
+++ user/krutonium-hm-extras/xdg.nix
@@ -10,7 +10,26 @@
         "application/x-7z-compressed" = [ "org.gnome.FileRoller.desktop" ];
         "application/x-compressed-tar" = [ "org.gnome.FileRoller.desktop" ];
         "application/zip" = [ "org.gnome.FileRoller.desktop" ];
+        "application/pdf" = [ "firefox.desktop" ];
+        "application/json" = [ "org.gnome.TextEditor.desktop" ];
+        "application/x-cd-image" = [ "gnome-disk-image-mounter.desktop" ];
+        "application/x-rar" = [ "org.gnome.FileRoller.desktop" ];
+        "application/x-ms-dos-executable" = [ "wine.desktop" ];
+
         "text/html" = [ "firefox.desktop" ];
+        "text/plain" = [ "org.gnome.TextEditor.desktop" ];
+
+        "image/png" = [ "org.gnome.eog.desktop" ];
+        "image/jpeg" = [ "org.gnome.eog.desktop" ];
+        "image/gif" = [ "org.gnome.eog.desktop" ];
+
+        "audio/mpeg" = [ "vlc.desktop" ];
+        "audio/flac" = [ "vlc.desktop" ];
+
+        "video/mp4" = [ "vlc.desktop" ];
+        "video/quicktime" = [ "vlc.desktop" ];
+
+        "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
       };
     };
   };
