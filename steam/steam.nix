3c7811c8c06bc5ea191b40eac1a0a66bd76dc7a0
Krutonium <PFCKrutonium@gmail.com>
2023-12-02 19:45:38 -0500

    VR Work

--- steam/steam.nix
+++ steam/steam.nix
@@ -29,75 +29,82 @@ with lib;
 with builtins;
 let
   cfg = config.sys.steam;
-  open_composite = pkgs.stdenv.mkDerivation rec {
-    pname = "open-composite";
-    version = "git";
-    src = pkgs.fetchFromGitLab {
-      owner = "znixian";
-      repo = "OpenOVR";
-      rev = "c5256a117f82c04e3f74cc8b3e2eb357f1425270";
-      sha256 = "sha256-0Es5FuEwu0L43VOYGdNBfxuBehlNx35ymjBUOG/pKLU=";
-      fetchSubmodules = true;
-    };
+  #open_composite = pkgs.stdenv.mkDerivation rec {
+  #  pname = "open-composite";
+  #  version = "git";
+  #  src = pkgs.fetchFromGitLab {
+  #    owner = "znixian";
+  #    repo = "OpenOVR";
+  #    rev = "c5256a117f82c04e3f74cc8b3e2eb357f1425270";
+  #    sha256 = "sha256-0Es5FuEwu0L43VOYGdNBfxuBehlNx35ymjBUOG/pKLU=";
+  #    fetchSubmodules = true;
+  #  };
 
     # disable all warnings (they become errors)
-    NIX_CFLAGS_COMPILE = "-Wno-error -w";
+   # NIX_CFLAGS_COMPILE = "-Wno-error -w";
 
-    nativeBuildInputs = with pkgs;[
-      cmake
-    ];
+    #nativeBuildInputs = with pkgs;[
+    #  cmake
+    #];
 
-    buildInputs = with pkgs.unstable; [
-      vulkan-loader
-      vulkan-headers
-      libGLU
-      python39
-      xorg.libX11
-    ];
+    #buildInputs = with pkgs.unstable; [
+    #  vulkan-loader
+    #  vulkan-headers
+    #  libGLU
+    #  python39
+    #  xorg.libX11
+    #];
 
-    enableParallelBuilding = true;
+    #enableParallelBuilding = true;
 
-    installPhase = ''
-      cp -r . $out
-    '';
-  };
+    #installPhase = ''
+    #  cp -r . $out
+    #'';
+  #};
 
   patch-vr = pkgs.writeShellScriptBin "patch-vr" ''
+    rm -rf ~/monado
+    mkdir ~/monado
+    cp -r /etc/monado/ ~/monado
     steam-run ~/.local/share/Steam/steamapps/common/SteamVR/bin/vrpathreg.sh removedriverswithname monado
-    steam-run ~/.local/share/Steam/steamapps/common/SteamVR/bin/vrpathreg.sh adddriver ${monado}/share/steamvr-monado
+    steam-run ~/.local/share/Steam/steamapps/common/SteamVR/bin/vrpathreg.sh adddriver ~/monado/share/steamvr-monado
+    #steam-run ~/.local/share/Steam/steamapps/common/SteamVR/bin/vrpathreg.sh adddriver ${pkgs.monado}/share/
+    #/etc/monado/scripts/get-ht-models.sh
   '';
 
-  monado = (pkgs.unstable.monado.overrideAttrs (old: {
-    src = pkgs.unstable.fetchFromGitLab {
-      domain = "gitlab.freedesktop.org";
-      owner = "monado";
-      repo = "monado";
-      rev = "9293c628d78ba595918b6b21460fc1df2fbd6f45";
-      sha256 = "sha256-oDYyrO45TBl8sTcjk6okMJ5vpqGA08h0XJTcf7grnfo=";
-    };
-  }));
+ # monado = (pkgs.unstable.monado.overrideAttrs (old: {
+ #   src = pkgs.unstable.fetchFromGitLab {
+ #     domain = "gitlab.freedesktop.org";
+ #     owner = "monado";
+ #     repo = "monado";
+ #     rev = "9293c628d78ba595918b6b21460fc1df2fbd6f45";
+ #     sha256 = "sha256-oDYyrO45TBl8sTcjk6okMJ5vpqGA08h0XJTcf7grnfo=";
+ #   };
+ # }));
 
   rift_s_udev = pkgs.writeTextFile {
     name = "moonlander-udev-rules";
     text = ''
       # Skip if a remove
-      ACTION=="remove", GOTO="xrhardware_end"
+      #ACTION=="remove", GOTO="xrhardware_end"
 
       # Oculus Rift S - USB
-      ATTRS{idVendor}=="2833", ATTRS{idProduct}=="0051", TAG+="uaccess", ENV{ID_xrhardware}="1"
-
+      #ATTRS{idVendor}=="2833", ATTRS{idProduct}=="0051", TAG+="uaccess", ENV{ID_xrhardware}="1"
+      #KERNEL=="hidraw*", ATTRS{idVendor}=="2833", MODE="0666", GROUP="users"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0666", GROUP="users"
+      SUBSYSTEM=="usb", ATTR{idVendor}=="2833", MODE="0666", GROUP="users"
       # Exit if we didn't find one
-      ENV{ID_xrhardware}!="1", GOTO="xrhardware_end"
+      #ENV{ID_xrhardware}!="1", GOTO="xrhardware_end"
 
       # XR devices with serial ports aren't modems, modem-manager
-      ENV{ID_xrhardware_USBSERIAL_NAME}!="", SUBSYSTEM=="usb", ENV{ID_MM_DEVICE_IGNORE}="1"
+      #ENV{ID_xrhardware_USBSERIAL_NAME}!="", SUBSYSTEM=="usb", ENV{ID_MM_DEVICE_IGNORE}="1"
 
       # Make friendly symlinks for XR USB-Serial devices.
-      ENV{ID_xrhardware_USBSERIAL_NAME}!="", SUBSYSTEM=="tty", SYMLINK+="ttyUSB.$env{ID_xrhardware_USBSERIAL_NAME}"
+      #ENV{ID_xrhardware_USBSERIAL_NAME}!="", SUBSYSTEM=="tty", SYMLINK+="ttyUSB.$env{ID_xrhardware_USBSERIAL_NAME}"
 
-      LABEL="xrhardware_end"
+      #LABEL="xrhardware_end"
     '';
-    destination = "/lib/udev/rules.d/70-xrhardware.rules";
+    destination = "/etc/udev/rules.d/70-xrhardware.rules";
   };
 in
 {
@@ -119,6 +126,9 @@ in
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
+    };
+    environment.etc."monado" = {
+      source = pkgs.monado;
     };
   };
 }
