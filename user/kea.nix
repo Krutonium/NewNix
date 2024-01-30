600605fae5e537881611c55cfe00789ff84d0378
Krutonium <PFCKrutonium@gmail.com>
2024-01-30 14:01:29 -0500

    Add new ssh key to kea

--- user/kea.nix
+++ user/kea.nix
@@ -15,6 +15,7 @@ in
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIElh2sqH+6bZWIDBL5haoTn0VPuFmHoROpernmr7Jrao kea@TWIST"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEOOQoLq0Ao5GuNSWDbxPuL/qYCTlaLL62BnyM/K+Xlt kea@BOX"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+EnBu0D+Nh20+Hyp90H96p4q5SsMZupHNH71v0NJUt kea@PASCIENCE"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQYEAnii3cGo9zehJKaHx0Z9xwQ2mTVz/TCvoWXeVjQ kea@venus"
       ];
       extraGroups = [ "wheel" ];
     };
