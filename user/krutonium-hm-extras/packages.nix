6c2767c9df52883c7f2e4c4fe43fd087e81e76fe
Krutonium <PFCKrutonium@gmail.com>
2024-02-25 20:01:28 -0500

    Workloads in theory work now!

--- user/krutonium-hm-extras/packages.nix
+++ user/krutonium-hm-extras/packages.nix
@@ -15,6 +15,9 @@ let
          for i in $out/sdk/*
          do
            i=$(basename $i)
+           length=$(printf "%s" "$i" | wc -c)
+           substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
+           i="$substring""00"
            mkdir -p $out/metadata/workloads/''${i/-*}
            touch $out/metadata/workloads/''${i/-*}/userlocal
         done
