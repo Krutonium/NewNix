abbcef01448bd100ce90501bce6507c8d7ac9241
Krutonium <PFCKrutonium@gmail.com>
2024-02-27 05:09:38 -0500

    Add Automounting of Steamdeck

--- devices/uMsiLaptop-hw.nix
+++ devices/uMsiLaptop-hw.nix
@@ -45,6 +45,28 @@
           "reconnect"
         ];
     };
+  fileSystems."/SteamDeck" =
+    {
+      device = "deck@10.0.0.9:/";
+      fsType = "sshfs";
+      options =
+        [
+          "allow_other" # for non-root access
+          "default_permissions"
+          "idmap=user"
+          "_netdev" # requires network to mount
+          "x-systemd.automount" # mount on demand
+          "uid=1000" # id -a
+          "gid=100"
+          "compression=yes" # Compression should be fine given thehost machine
+          "max_conns=20" # MOAR THREADS (when needed)
+          "IdentityFile=/home/krutonium/.ssh/id_ed25519"
+          # Handle connection drops better
+          "ServerAliveInterval=2"
+          "ServerAliveCountMax=2"
+          "reconnect"
+        ];
+    };
 
   swapDevices = [ ];
 
