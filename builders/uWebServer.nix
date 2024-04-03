ab725b51cf480ba21b4693a57aa51bb66023849d
Krutonium <PFCKrutonium@gmail.com>
2024-04-03 04:35:21 -0400

    20 Jobs

--- builders/uWebServer.nix
+++ builders/uWebServer.nix
@@ -15,7 +15,7 @@
     # if the builder supports building for multiple architectures,
     # replace the previous line by, e.g.
     # systems = ["x86_64-linux" "aarch64-linux"];
-    maxJobs = 4;
+    maxJobs = 20;
     speedFactor = 2;
     supportedFeatures = [
       "nixos-test"
