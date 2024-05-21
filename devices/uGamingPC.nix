0c534f27728348ac494075f91db00d48274cbd40
Krutonium <PFCKrutonium@gmail.com>
2024-05-21 18:55:28 -0400

    nVidia 555

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -1,7 +1,15 @@
 { config, pkgs, channels, ... }:
 let
   kernel = with pkgs; unstable.linuxPackages_latest;
-  video = config.boot.kernelPackages.nvidiaPackages.latest;
+  #video = config.boot.kernelPackages.nvidiaPackages.latest;
+  video = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "555.42.02";
+    sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+    sha256_aarch64 = lib.fakeSha256;
+    openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+    settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+    persistencedSha256 = lib.fakeSha256;
+  };
   zenpower = config.boot.kernelPackages.zenpower;
   ddcutil = config.boot.kernelPackages.ddcci-driver.overrideAttrs (old: {
     patches = [
