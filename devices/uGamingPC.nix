e9cca01ee4d2bffd4ef0f28666dd866c0b154564
Krutonium <PFCKrutonium@gmail.com>
2024-05-25 14:08:37 -0400

    Switch to 555 Official

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -1,15 +1,15 @@
 { config, pkgs, channels, lib, ... }:
 let
   kernel = with pkgs; unstable.linuxPackages_latest;
-  #video = config.boot.kernelPackages.nvidiaPackages.latest;
-  video = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "555.42.02";
-    sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-    sha256_aarch64 = "sha256-ekx0s0LRxxTBoqOzpcBhEKIj/JnuRCSSHjtwng9qAc0=";
-    openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-    settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-    persistencedSha256 = "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
-  };
+  video = config.boot.kernelPackages.nvidiaPackages.beta;
+  #video = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+  #  version = "555.42.02";
+  #  sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+  #  sha256_aarch64 = "sha256-ekx0s0LRxxTBoqOzpcBhEKIj/JnuRCSSHjtwng9qAc0=";
+  #  openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+  #  settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+  #  persistencedSha256 = "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
+  #};
   zenpower = config.boot.kernelPackages.zenpower;
   ddcutil = config.boot.kernelPackages.ddcci-driver.overrideAttrs (old: {
     patches = [
