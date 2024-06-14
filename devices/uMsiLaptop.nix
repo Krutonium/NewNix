c1c2d2051a57a5a5d7a13f9ce0d2ea7f2dc08ff4
Krutonium <PFCKrutonium@gmail.com>
2024-06-14 03:47:46 -0400

    Changes

--- devices/uMsiLaptop.nix
+++ devices/uMsiLaptop.nix
@@ -13,10 +13,10 @@ in
 {
   boot.initrd.systemd.enable = true;
   boot.kernelPackages = kernel;
-  #boot.initrd.availableKernelModules = [ "nvidia" ];
+  #boot.initrd.availableKernelModules = [ "" ];
   networking.hostName = Hostname;
   networking.firewall.interfaces."enp3s0".allowedUDPPorts = [ 67 ];
-  #boot.kernelParams = [ "nouveau.config=NvClkMode=15" ];
+  boot.kernelParams = [ "nouveau.config=NvClkMode=15" ];
   boot.loader.grub.gfxmodeEfi = "1920x1080";
   boot.loader.grub.gfxpayloadEfi = "keep";
   environment.systemPackages = [ 
