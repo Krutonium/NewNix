5061ccfb2d60da0c6024e39c6f9fd07e4eb6ca86
Krutonium <PFCKrutonium@gmail.com>
2024-07-04 08:06:27 -0400

    Add unity hub

--- user/krutonium-hm-extras/packages-desktop.nix
+++ user/krutonium-hm-extras/packages-desktop.nix
@@ -84,6 +84,7 @@ in
     #(pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.rider [ "14839" "17718" "13882" ])
     (pkgs.unstable.jetbrains.plugins.addPlugins pkgs.unstable.jetbrains.rider [ "github-copilot" ])
     (pkgs.unstable.jetbrains.plugins.addPlugins pkgs.unstable.jetbrains.idea-ultimate [ "github-copilot" ])
+    pkgs.unityhub
 
     #rider
     #riderScript
