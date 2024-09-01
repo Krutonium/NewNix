572362fc04b44f9a5e6d0e266ecd151e7df8c6ce
Krutonium <PFCKrutonium@gmail.com>
2024-09-01 18:12:35 -0400

    nVidia Fix?

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -1,7 +1,7 @@
 { config, pkgs, channels, lib, ... }:
 let
   kernel = with pkgs; unstable.linuxPackages_zen;
-  video = config.boot.kernelPackages.nvidiaPackages.beta;
+  video = config.boot.kernelPackages.nvidiaPackages.stable;
   #video = config.boot.kernelPackages.nvidiaPackages.mkDriver {
   #  version = "555.42.02";
   #  sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
@@ -25,7 +25,7 @@ in
   hardware.firmware = [ video.firmware ];
   boot = {
     kernelPackages = kernel;
-    kernelParams = [ "amd_iommu=on" "iommu=pt" "nvidia-drm.modeset=1" ];
+    kernelParams = [ "amd_iommu=on" ];
     tmp.useTmpfs = false;
     loader.grub = {
       gfxmodeEfi = "1920x1080";
@@ -114,7 +114,9 @@ in
     package = video;
     open = false; #some day my beauty
     nvidiaSettings = true;
+    modesetting = true;
   };
+  hardware.graphics.enable = true;
   #boot.initrd.availableKernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
   boot.extraModulePackages = with config.boot.kernelPackages;
     [
