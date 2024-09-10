00e1575b5cf227868ee797849079fad8f1c9f540
Krutonium <PFCKrutonium@gmail.com>
2024-09-10 19:55:32 -0400

    Rewrite Flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; # NixOS release channel
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # NixOS unstable channel
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master"; # NixOS hardware channel
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05"; # Home Manager release channel
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     update = {
@@ -26,144 +26,100 @@
     };
     nur = {
       url = "github:nix-community/NUR";
-      # inputs.nixpkgs.follows = "nixpkgs"; NUR does not.
     };
     bcachefs-tools.url = "github:koverstreet/bcachefs-tools";
   };
+
   outputs = { self, nixpkgs, nixpkgs-unstable, nixpkgs-master, nixos-hardware, home-manager, update, nix-monitored, nixd, fan-controller, nur, bcachefs-tools, ... }@inputs:
     let
-      # This is a Generic Block of St00f
       system = "x86_64-linux";
-      genericModules = [
+
+      # Generic Modules
+      baseModules = [
         ./common.nix
         {
-          # This fixes things that don't use Flakes, but do want to use NixPkgs.
-          nix.registry.nixos.flake = inputs.self;
+          nix.registry.nixos.flake = self;
           environment.etc."nix/inputs/nixpkgs".source = nixpkgs.outPath;
           nix.nixPath = [ "nixpkgs=${nixpkgs.outPath}" ];
         }
-        home-manager.nixosModules.home-manager
-        {
-          nix.registry.nixos.flake = inputs.self;
+        home-manager.nixosModules.home-manager {
+          nix.registry.nixos.flake = self;
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
         }
-
-        # Make sure you add Overlays here
-        ({ config, pkgs, ... }:
-          {
-            nixpkgs.overlays =
-              [
-                overlay-unstable
-                overlay-master
-                overlay-nixpkgs-update
-                # overlay-monitored
-                nixd.overlays.default
-                overlay-fanController
-                nur.overlay
-                bcache_overlay
-              ];
-          }
-        )
       ];
-      # Overlays
-      bcache_overlay = final: prev: {
-        bcachefs-tools = inputs.bcachefs-tools.packages.x86_64-linux.bcachefs-tools;
-      };
-      # nixpkgs-unstable
-      overlay-unstable = final: prev: {
-        unstable = import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-          config.nvidia.acceptLicense = true;
+
+      # Overlay Definitions
+      overlays = {
+        bcachefs = final: prev: {
+          bcachefs-tools = inputs.bcachefs-tools.packages.x86_64-linux.bcachefs-tools;
+        };
+        unstable = final: prev: {
+          unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; config.nvidia.acceptLicense = true; };
+        };
+        master = final: prev: {
+          master = import nixpkgs-master { inherit system; config.allowUnfree = true; config.nvidia.acceptLicense = true; };
+        };
+        nixpkgsUpdate = final: prev: {
+          nixpkgs-update = update.defaultPackage.x86_64-linux;
+        };
+        fanController = self: super: {
+          BetterFanController = fan-controller.defaultPackage.x86_64-linux;
+        };
+        monitored = self: super: {
+          nixos-rebuild = super.nixos-rebuild.override { nix = self.nix-monitored; };
+          nix-direnv = super.nix-direnv.override { nix = self.nix-monitored; };
+          nix-monitored = inputs.nix-monitored.packages.${self.system}.default.override self;
         };
       };
-      # nixpkgs-master
-      overlay-master = final: prev: {
-        master = import nixpkgs-master {
-          inherit system;
-          config.allowUnfree = true;
-          config.nvidia.acceptLicense = true;
-        };
+
+      genericModulesWithOverlays = baseModules ++ [
+        ({ config, pkgs, ... }: {
+          nixpkgs.overlays = [
+            overlays.unstable
+            overlays.master
+            overlays.nixpkgsUpdate
+            nixd.overlays.default
+            overlays.fanController
+            nur.overlay
+            overlays.bcachefs
+          ];
+        })
+      ];
+
+      # NixOS Configuration Helper
+      nixosConfiguration = name: deviceConfig: nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = genericModulesWithOverlays ++ (with nixos-hardware.nixosModules; deviceConfig);
+        specialArgs.channels = { inherit nixpkgs nixpkgs-unstable; };
       };
-      # overlay for nixpkgs-update
-      overlay-nixpkgs-update = final: prev: {
-        nixpkgs-update = update.defaultPackage.x86_64-linux;
-      };
-      # Fan Controller for AMD Devices
-      overlay-fanController = self: super: {
-        BetterFanController = fan-controller.defaultPackage.x86_64-linux;
-      };
-      overlay-monitored = self: super: {
-        nixos-rebuild = super.nixos-rebuild.override {
-          nix = self.nix-monitored;
-        };
-        nix-direnv = super.nix-direnv.override {
-          nix = self.nix-monitored;
-        };
-        nix-monitored = inputs.nix-monitored.packages.${self.system}.default.override self;
-      };
-    in
-    {
+
+      # Common Device Modules
+      commonPCModules = with nixos-hardware.nixosModules; [ common-pc common-pc-ssd ];
+      commonIntel = with nixos-hardware.nixosModules; [ common-cpu-intel ];
+      commonAMD = with nixos-hardware.nixosModules; [ common-cpu-amd ];
+      commonLaptop = with nixos-hardware.nixosModules; [ common-pc-laptop ];
+      
+    in {
       ##################
       ### uWebServer ###
       ##################
-      nixosConfigurations.uWebServer = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = genericModules ++ (with nixos-hardware.nixosModules; [
-          common-pc
-          common-pc-ssd
-          common-cpu-intel
-        ]) ++ [ ./devices/uWebServer.nix ];
-      };
+      nixosConfigurations.uWebServer = nixosConfiguration "uWebServer" (commonPCModules ++ commonIntel ++ [ ./devices/uWebServer.nix ]);
+
       #################
       ### uGamingPC ###
       #################
-      nixosConfigurations.uGamingPC = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = genericModules ++ (with nixos-hardware.nixosModules; [
-          common-pc
-          common-pc-ssd
-          common-cpu-amd
-        ]) ++ [ ./devices/uGamingPC.nix ];
-        specialArgs.channels = { inherit nixpkgs nixpkgs-unstable; };
-      };
+      nixosConfigurations.uGamingPC = nixosConfiguration "uGamingPC" (commonPCModules ++ commonAMD ++ [ ./devices/uGamingPC.nix ]);
+
       ##################
       ### uMsiLaptop ###
       ##################
-      nixosConfigurations.uMsiLaptop = nixpkgs.lib.nixosSystem {
-        #deploy#https://github.com/Skulltrail192/One-Core-API-Binaries/archive/refs/heads/master.zip
-        inherit system;
-        modules = genericModules ++ (with nixos-hardware.nixosModules; [
-          common-pc
-          common-pc-ssd
-          common-pc-laptop
-          common-cpu-intel
-        ]) ++ [ ./devices/uMsiLaptop.nix ];
-      };
+      nixosConfigurations.uMsiLaptop = nixosConfiguration "uMsiLaptop" (commonPCModules ++ commonLaptop ++ commonIntel ++ [ ./devices/uMsiLaptop.nix ]);
+
       #################
-      ### uHPLaptop ###
+      ### Uncomment if needed ###
       #################
-      #nixosConfigurations.uHPLaptop = nixpkgs.lib.nixosSystem {
-      #  inherit system;
-      #  modules = genericModules ++ (with nixos-hardware.nixosModules; [
-      #    common-pc
-      #    common-pc-ssd
-      #    common-pc-laptop
-      #    common-cpu-intel
-      #  ]) ++ [ ./devices/uHPLaptop.nix ];
-      #};
-      ###################
-      ### uMacBookPro ###
-      ###################
-      #nixosConfigurations.uMacBookPro = nixpkgs.lib.nixosSystem {
-      #  inherit system;
-      #  modules = genericModules ++ (with nixos-hardware.nixosModules; [
-      #    common-pc
-      #    common-pc-ssd
-      #    common-pc-laptop
-      #    common-cpu-intel
-      #  ]) ++ [ ./devices/uMacBookPro.nix ];
-      #};
+      #nixosConfigurations.uHPLaptop = nixosConfiguration "uHPLaptop" (commonPCModules ++ commonLaptop ++ commonIntel ++ [ ./devices/uHPLaptop.nix ]);
+      #nixosConfigurations.uMacBookPro = nixosConfiguration "uMacBookPro" (commonPCModules ++ commonLaptop ++ commonIntel ++ [ ./devices/uMacBookPro.nix ]);
     };
 }
