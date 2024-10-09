257a8e11992b2e093ab8c2b8a6b8c140a99288d9
Krutonium <PFCKrutonium@gmail.com>
2024-10-09 14:25:59 -0400

    Disable Sleep and Hibernate

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -38,6 +38,12 @@ in
     pkgs.logitech-udev-rules
     pkgs.via
   ];
+  # Disable Sleep/Hibernate System Wide
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   services.flatpak.enable = true;
   services.ratbagd.enable = true;
   systemd.services."mount-games" = {
