49bd78e67821929cc5366cbf6fbacc77e1a2ddf9
Krutonium <PFCKrutonium@gmail.com>
2024-12-18 17:35:38 -0500

    Attempt at IPv6

--- devices/uWebServer-networking.nix
+++ devices/uWebServer-networking.nix
@@ -1,91 +1,102 @@
 { config, pkgs, lib, ... }:
 {
+  # Enable IPv4 and IPv6 packet forwarding and IPv6 autoconfiguration
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.forwarding" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
-    "net.ipv6.conf.all.accept_ra" = 2;
-    "net.ipv6.conf.all.request_prefix" = 1;
-    "net.ipv6.conf.all.autoconf" = 1;
-    "net.ipv6.conf.all.use_tempaddr" = 0;
+    "net.ipv6.conf.all.forwarding" = 1; 
+    "net.ipv6.conf.all.accept_ra" = 2;    # Accept router advertisements even when forwarding is enabled
+    "net.ipv6.conf.all.request_prefix" = 1; # Request prefix delegation
+    "net.ipv6.conf.all.autoconf" = 1;     # Enable stateless autoconfiguration
+    "net.ipv6.conf.all.use_tempaddr" = 0; # Disable temporary addresses
   };
-  #Set up network interfaces to have *actually reliable names* (WOW!)
+
+  # Map network interfaces to consistent names based on MAC addresses
   services = {
     udev.extraRules = ''
       ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="40:8d:5c:54:89:96", NAME="WAN"
       ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="ac:16:2d:9a:17:c0", NAME="LAN0"
-      ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="ac:16:2d:9a:17:c1", NAME="LAN1"
+      ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="ac:16:2d:9a:17:c1", NAME="LAN1" 
       ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="ac:16:2d:9a:17:c2", NAME="LAN2"
       ACTION=="add", SUBSYSTEM=="net", ATTR{address}=="ac:16:2d:9a:17:c3", NAME="LAN3"
     '';
   };
+
+  # Enable nftables firewall
   networking.nftables.enable = true;
-  networking.firewall.allowedUDPPorts = [ 546 ]; #DHCPv6-PD
+  networking.firewall.allowedUDPPorts = [ 546 ]; # Allow DHCPv6 client
   networking.firewall.trustedInterfaces = [ "br0" ];
+
+  # Configure systemd-networkd
   systemd.network = {
     enable = true;
     networks = {
       "wan" = {
         matchConfig.PermanentMACAddress = "40:8d:5c:54:89:96";
         networkConfig = {
-          #IPv6AcceptRA = true;
+          IPv6AcceptRA = true;
           DHCP = "yes";
         };
         linkConfig = {
           RequiredForOnline = "routable";
         };
-        #dhcpV6Config = {
-        #  WithoutRA = "solicit";
-        #  PrefixDelegationHint = "::/64";
-        #};
-        #ipv6SendRAConfig = {
-        #  Managed = true;
-        #};
-        #ipv6AcceptRAConfig = {
-        #  DHCPv6Client = "yes";
-        #};
+        dhcpV6Config = {
+          WithoutRA = "solicit";
+          PrefixDelegationHint = "::/64";
+        };
+        ipv6SendRAConfig = {
+          Managed = true;
+        };
+        ipv6AcceptRAConfig = {
+          DHCPv6Client = "yes";
+        };
       };
       "br0" = {
         matchConfig.Name = "br0";
         networkConfig = {
           DHCPPrefixDelegation = true;
-          #IPv6SendRA = true;
-          #IPv6AcceptRA = true;
+          IPv6SendRA = true;
+          IPv6AcceptRA = true;
         };
-        #ipv6SendRAConfig = {
-        #  EmitDNS = true;
-        #  DNS = "2001:4860:4860::8888";
-        #};
+        ipv6SendRAConfig = {
+          EmitDNS = true;
+          DNS = "2001:4860:4860::8888"; # Google IPv6 DNS
+        };
       };
     };
   };
+
+  # Disable systemd-resolved as we're using dnsmasq
   services.resolved.enable = false;
+
   networking = {
-    # We're not using NetworkManager, as it apparently kinda just dies when given access to the raw internet.
+    # Disable NetworkManager in favor of systemd-networkd
     networkmanager.enable = lib.mkForce false;
     useNetworkd = true;
-    #nameservers = [ "10.0.0.1" ]; #Configured in Common
 
+    # Create bridge interface for LAN ports
     bridges = {
       "br0" = {
         interfaces = [ "LAN0" "LAN1" "LAN2" "LAN3" ];
-        # All ports on the Card are part of the LAN
       };
     };
+
+    # Configure NAT for IPv4 and IPv6
     nat = {
       enable = true;
       externalInterface = "WAN";
       internalInterfaces = [ "br0" ];
       internalIPs = [ "10.0.0.0/24" ];
-      #enableIPv6 = true;
-      #internalIPv6s = [ "2001:db8:1234:5678::/64" ];
+      enableIPv6 = true;
+      # This will be automatically configured from prefix delegation
+      internalIPv6s = [ "::/64" ];
     };
+
+    # Configure network interfaces
     interfaces = {
       "WAN" = {
-        #We're getting the IP dynamically from the ISP.
-        useDHCP = true;
+        useDHCP = true; # Get IPv4 and IPv6 configuration from ISP
       };
       "br0" = {
-        # For now we're setting this statically, but I don't think there is any reason we couldn't use DHCP here.
         ipv4.addresses = [{ address = "10.0.0.1"; prefixLength = 24; }];
         useDHCP = false;
         macAddress = "ac:16:2d:9a:17:c5";
@@ -94,53 +105,56 @@
     tempAddresses = "disabled";
   };
 
+  # Enable IPv6 router advertisements
   services.radvd = {
-    enable = false;
+    enable = true;
     config = ''
       interface br0 {
         AdvSendAdvert on;
-        prefix 2001:db8:1234:5678::/64 { };
+        # This prefix will be automatically configured from delegation
+        prefix ::/64 {
+          AdvOnLink on;
+          AdvAutonomous on;
+        };
       };
     '';
   };
 
+  # Configure firewall to allow DNS and DHCP on LAN
   networking.firewall.interfaces."br0".allowedTCPPorts = [ 53 67 ];
   networking.firewall.interfaces."br0".allowedUDPPorts = [ 53 67 ];
+
+  # Configure DHCP and DNS server
   services.dnsmasq = {
     enable = true;
     alwaysKeepRunning = true;
     settings = {
       interface = "br0";
-      # Domain
+      # Local domain configuration
       domain = "krutonium.ca,10.0.0.1";
-      # DHCP Range, 5 Minute expire
+      # DHCP range with 5 minute lease time
       dhcp-range = "10.0.0.1,10.0.0.254,5m";
-      # DHCP Options: Router Advertise at 10.0.0.1, and a static route at 10.0.0.1 for internet access
-      dhcp-option = [ "option:router,10.0.0.1" "option:classless-static-route,10.0.0.0/24,10.0.0.1" ];
-      # Statically Allocated Addresses
-      dhcp-host = [
-        # uWebServer
-        "ac:16:2d:9a:17:c5,10.0.0.1"
-        # uGamingPC / Linux
-        "18:C0:4D:04:05:E7,10.0.0.2"
-        # uWindowsPC / Gaming/Windows
-        "30:9c:23:d3:06:fd,10.0.0.3"
-        # uMsiLaptop
-        "F8:16:54:A5:A5:91,10.0.0.4"
-        # Archer Router
-        "14:EB:B6:58:A1:D4,10.0.0.7"
-        # Printer
-        "b0:68:e6:97:f4:37,10.0.0.8"
-        # SteamDeck
-        "50:5A:65:61:DB:3B,10.0.0.9"
-        # 5 and 6 intentionally missing for now.
+      # Configure router and static routes
+      dhcp-option = [ 
+        "option:router,10.0.0.1" 
+        "option:classless-static-route,10.0.0.0/24,10.0.0.1" 
       ];
-      # Listens to br0
+      # Static DHCP assignments
+      dhcp-host = [
+        "ac:16:2d:9a:17:c5,10.0.0.1"    # uWebServer
+        "18:C0:4D:04:05:E7,10.0.0.2"    # uGamingPC / Linux
+        "30:9c:23:d3:06:fd,10.0.0.3"    # uWindowsPC / Gaming/Windows
+        "F8:16:54:A5:A5:91,10.0.0.4"    # uMsiLaptop
+        "14:EB:B6:58:A1:D4,10.0.0.7"    # Archer Router
+        "b0:68:e6:97:f4:37,10.0.0.8"    # Printer
+        "50:5A:65:61:DB:3B,10.0.0.9"    # SteamDeck
+      ];
+      # Listen on all local addresses
       listen-address="::1,127.0.0.1,10.0.0.1";
-      expand-hosts = true; #I *think* that's how that'd work?
-      # DNS Servers:
-      server = [ "1.1.1.1" "8.8.8.8" ]; # If both are down, an apocalypse is occuring.
-      # Routes traffic to my domain to my server
+      expand-hosts = true;
+      # Upstream DNS servers
+      server = [ "1.1.1.1" "8.8.8.8" ];
+      # Local DNS overrides
       address = [ 
         "/krutonium.ca/10.0.0.1" 
         "/BRWB068E697F437.local/10.0.0.8" 
