bca4480a60f3d88284935f17be8f3bde36eaf7f5
Krutonium <PFCKrutonium@gmail.com>
2024-12-18 17:23:07 -0500

    Rebuild common.nix

--- common.nix
+++ common.nix
@@ -14,120 +14,142 @@
     ./scripts.nix
     #./builders
   ];
+  # PAM configuration for GNOME keyring auto-unlock
   security.pam.services = {
     gdm-autologin-keyring.text = ''
       auth      optional      ${pkgs.gdm}/lib/security/pam_gdm.so
       auth      optional      ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
     '';
   };
-  services.irqbalance.enable = true;
-  #xdg.portal.enable = true;
-  #xdg.portal.extraPortals = [
-  #  pkgs.xdg-desktop-portal-gtk
-  #];
-  environment.shellAliases = {
-    ls = "${pkgs.eza}/bin/eza --icons --git";
-  };
-  services.system76-scheduler = {
-    enable = true;
-    settings.processScheduler = {
-      pipewireBoost.enable = true;
+
+  # System services configuration
+  services = {
+    # CPU interrupt balancing
+    irqbalance.enable = true;
+
+    # Process scheduler for better performance
+    system76-scheduler = {
       enable = true;
+      settings.processScheduler = {
+        pipewireBoost.enable = true;
+        enable = true;
+      };
+      package = pkgs.system76-scheduler;
     };
-    package = pkgs.system76-scheduler;
+
+    # Printer support
+    printing = {
+      enable = true;
+      drivers = with pkgs; [ brlaser ];
+    };
+
+    # Monitor control support
+    ddccontrol.enable = true;
+
+    # Firmware update daemon
+    fwupd.enable = true;
+
+    # udev rules for disk I/O ratios
+    udev.extraRules = ''
+      ACTION=="add|change", SUBSYSTEM=="bdi", ATTR{min_ratio}="2", ATTR{max_ratio}="50"
+    '';
   };
 
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
-  ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-  '';
-  security.polkit.enable = true;
-  services.udev.extraRules = ''
-    ACTION=="add|change", SUBSYSTEM=="bdi", ATTR{min_ratio}="2", ATTR{max_ratio}="50"
-  '';
-  security.sudo.wheelNeedsPassword = false;
-  services.printing.enable = true;
-  services.printing.drivers = with pkgs; [ brlaser ];
-  services.ddccontrol.enable = true;
-  environment.variables = {
-    NIXPKGS_ALLOW_UNFREE = "1";
-    MANGOHUD = "1";
-    QT_QPA_PLATFORMTHEME = "gnome";
-  };
-  boot = {
-    tmp.cleanOnBoot = true;
-    kernel = {
-      sysctl = {
-        "vm.max_map_count" = 1000000;
-        "vm.dirty_ratio" = "25"; # 25% of all memory optionally as write cache
-        "kernel.panic" = "60";
-        "kernel.perf_event_paranoid" = "1";
-        "kernel.kptr_restrict" = "0";
-      };
+  # Shell configuration
+  environment = {
+    shellAliases = {
+      ls = "${pkgs.eza}/bin/eza --icons --git";
     };
-    supportedFilesystems = [ "ntfs" ]; #Add explicit NTFS support
+    variables = {
+      NIXPKGS_ALLOW_UNFREE = "1";
+      MANGOHUD = "1";
+      QT_QPA_PLATFORMTHEME = "gnome";
+    };
   };
-  documentation.enable = true;
+
+  # Boot configuration
+  boot = {
+    # Virtual webcam support
+    extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
+
+    # System maintenance and kernel parameters
+    tmp.cleanOnBoot = true;
+    kernel.sysctl = {
+      "vm.max_map_count" = 1000000;
+      "vm.dirty_ratio" = "25"; # 25% of all memory optionally as write cache
+      "kernel.panic" = "60";
+      "kernel.perf_event_paranoid" = "1";
+      "kernel.kptr_restrict" = "0";
+    };
+    supportedFilesystems = [ "ntfs" ]; # Add explicit NTFS support
+  };
+
+  # Security settings
+  security = {
+    polkit.enable = true;
+    sudo.wheelNeedsPassword = false;
+  };
+
+  # Hardware configuration
   hardware = {
     enableAllFirmware = false;
     bluetooth.enable = true;
     usb-modeswitch.enable = true;
   };
+
+  # Documentation
+  documentation.enable = true;
+
+  # Nix package manager configuration
   nix = {
     settings = {
       auto-optimise-store = true;
       trusted-users = [ "@wheel" ];
       min-free = 50 * 1000 * 1000 * 1000; # 50GB
+      system-features = [ "i686-linux" "x86_64-linux" "big-parallel" "kvm" ];
     };
     gc = {
       automatic = true;
       options = "--delete-older-than 7d";
       dates = "weekly";
     };
-    #package = pkgs.nix-monitored;
     extraOptions = ''
       experimental-features = nix-command flakes
       extra-platforms = x86_64-linux i686-linux aarch64-linux riscv64-linux
-      #download-buffer-size = "99999999"
     '';
-    settings = {
-      system-features = [ "i686-linux" "x86_64-linux" "big-parallel" "kvm" ];
-    };
   };
+
+  # System time configuration
   time = {
     hardwareClockInLocalTime = true;
     timeZone = "America/Toronto";
   };
+
+  # Package management configuration
   nixpkgs = {
     config = {
       allowUnfree = true;
       nvidia.acceptLicense = true;
       allowUnfreePredicate = (pkg: true);
       permittedInsecurePackages = [
-        #"electron-25.9.0"
-        "dotnet-sdk-wrapped-6.0.428"
-        # ReEvaluate @ 24.05
-        # Needed for YouTube Music
+        "dotnet-sdk-wrapped-6.0.428" # Needed for YouTube Music - ReEvaluate @ 24.05
       ];
     };
   };
+
+  # Network configuration
   networking = {
-    networkmanager = {
-      enable = true;
+    networkmanager.enable = true;
+    firewall.enable = true;
+    dhcpcd = {
+      wait = "background";
+      extraConfig = "noarp";
     };
-    firewall = {
-      enable = true;
-    };
-    dhcpcd.wait = "background";
-    dhcpcd.extraConfig = "noarp";
-    #nameservers = [
-    #  "8.8.8.8"
-    #  "1.1.1.1"
-    #  "2001:4860:4860::8888"
-    #  "2606:4700:4700::1111"
-    #];
     nameservers = [
       "10.0.0.1"
       "1.1.1.1"
@@ -135,60 +157,31 @@
     ];
   };
 
-  i18n = {
-    defaultLocale = "en_CA.UTF-8";
-  };
+  # Locale settings
+  i18n.defaultLocale = "en_CA.UTF-8";
 
-  #console = {
-  #  earlySetup = false;
-  #  packages = with pkgs; [ monocraft ];
-  #  font = "${pkgs.monocraft}/share/fonts/truetype/8ah1prg91rd6y6qz0bc18bk045s9l3q0-Monocraft-no-ligatures.ttf";
-  #  keyMap = "us";
-  #};
-
+  # Systemd configuration
   systemd = {
     enableEmergencyMode = false;
-    network = {
-      wait-online = {
-        anyInterface = true;
-      };
-    };
-    services = {
-      systemd-udev-settle.enable = true;
-    };
+    network.wait-online.anyInterface = true;
+    services.systemd-udev-settle.enable = true;
+    extraConfig = ''
+      DefaultLimitNOFILE=1048576
+      DefaultTimeoutStopSec=10s
+    '';
   };
 
-  services = {
-    fwupd.enable = true;
+  # Program configurations
+  programs = {
+    noisetorch.enable = true;
+    gamemode = {
+      enable = true;
+      enableRenice = true;
+    };
+    command-not-found.enable = false;
   };
-  programs.noisetorch.enable = true;
-  programs.gamemode = {
-    enable = true;
-    enableRenice = true;
-  };
-  programs.command-not-found.enable = false;
-  systemd.extraConfig = ''
-    DefaultLimitNOFILE=1048576
-    DefaultTimeoutStopSec=10s
-  '';
-  nixpkgs.overlays = [
-    (final: prev: {
-      mesa-fix =
-        let
-          nixpkgs-fix = builtins.fetchTarball {
-            url = "https://github.com/nixos/nixpkgs/tarball/19be5ac0119740b050ddcfd8608691ebf65abf9e";
-            sha256 = "0z38lf6gq8ciq5nlw9ziryi9j9klhwzz2xims10pgcwllbn3acw7";
-          };
-        in
-        (import nixpkgs-fix { inherit (pkgs) system; }).mesa;
-    })
-  ];
-  #hardware.opengl = {
-  #  package = pkgs.mesa-fix.drivers;
-  #  package32 = pkgs.pkgsi686Linux.mesa-fix.drivers;
-  #};
 
-  # DO NOT CHANGE THIS
+  # System state version (DO NOT CHANGE)
   system.stateVersion = "23.11";
 
 }
