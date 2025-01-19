1b1acce574f062eca5e0d9f054353d2f7d451680
Krutonium <PFCKrutonium@gmail.com>
2025-01-18 20:21:34 -0500

    JRE17

--- minecraft/Gryphon.nix
+++ minecraft/Gryphon.nix
@@ -29,7 +29,7 @@ in
         '';
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
-      path = [ pkgs.jdk23 pkgs.bash ];
+      path = [ pkgs.jdk17 pkgs.bash ];
       script =
         ''
           ./run.sh
