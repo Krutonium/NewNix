d1283edaa01edd9d0f1feb18e11f61621bd561ae
Krutonium <PFCKrutonium@gmail.com>
2025-01-24 14:06:12 -0500

    Switch to graalvm

--- minecraft/Gryphon.nix
+++ minecraft/Gryphon.nix
@@ -29,7 +29,7 @@ in
         '';
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
-      path = [ pkgs.jdk21 pkgs.bash ];
+      path = [ pkgs.graalvm-ce pkgs.bash ];
       script =
         ''
           ./run.sh
