c3361d1fd8d5e8508b0a78282079f0d68640c884
Krutonium <PFCKrutonium@gmail.com>
2025-01-25 02:27:42 -0500

    Better Name

--- services/update-schedule.nix
+++ services/update-schedule.nix
@@ -7,7 +7,7 @@ in
 {
   config = mkIf (cfg.schedule-updates == true) {
       # Define the script
-      systemd.services.odd-day-task = {
+      systemd.services.schedule-update = {
         description = "Update the system on odd days from Unix Epoch";
         serviceConfig = {
           Type = "oneshot";
@@ -33,9 +33,9 @@ in
       };
     
       # This will result in the updates happening at 4 AM every other day.
-      systemd.timers.odd-day-task = {
-        description = "Timer for odd-day-task service";
-        wants = [ "odd-day-task.service" ];
+      systemd.timers.schedule-update = {
+        description = "Timer for schedule-update service";
+        wants = [ "schedule-update.service" ];
         timerConfig = {
           OnCalendar = "*-*-* 04:00:00";
           Persistent = true;
@@ -43,6 +43,6 @@ in
       };
     
       # Ensure the timer is started and enabled
-      systemd.services."odd-day-task".wantedBy = [ "timers.target" ];
+      systemd.services."schedule-update".wantedBy = [ "timers.target" ];
   };
 }
