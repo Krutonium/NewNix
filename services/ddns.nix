aed6675c7109ce1b52af91249435a98106ff35d8
Krutonium <PFCKrutonium@gmail.com>
2025-02-04 12:51:44 -0500

    In theory, Domain Updates

--- services/ddns.nix
+++ services/ddns.nix
@@ -3,15 +3,56 @@ with lib;
 with builtins;
 let
   cfg = config.sys.services;
+  check-script = pkgs.writeShellScript "check-ip-change" ''
+    STATE_FILE=/persist/ip_state
+    AUTH_FILE=/persist/ddnsclient.auth
+    CURRENT_IP=$(curl -s --config "$AUTH_FILE" https://api64.ipify.org)
+
+    if [ -f "$STATE_FILE" ]; then
+      PREVIOUS_IP=$(cat "$STATE_FILE")
+      if [ "$CURRENT_IP" = "$PREVIOUS_IP" ]; then
+        exit 0
+      fi
+    fi
+
+    echo "$CURRENT_IP" > "$STATE_FILE"
+    PASSWORD=$(cat "$AUTH_FILE")
+    curl -s "https://dynamicdns.park-your-domain.com/update?domain=krutonium.ca&password=$PASSWORD&host=@"
+    curl -s "https://dynamicdns.park-your-domain.com/update?domain=krutonium.ca&password=$PASSWORD&host=*"
+  '';
 in
 {
   config = mkIf (cfg.ddns == true) {
-    services.ddclient = {
-      enable = true;
-      protocol = "namecheap";
-      username = "krutonium.ca";
-      domains = [ "*" "@" ];
-      passwordFile = "/persist/ddnsclient.auth";
+    systemd.services.update_domain = {
+      description = "Check External IP and Trigger cURL on Change";
+      after = [ "network-online.target" "sys-subsystem-net-devices-WAN.device" "network.target"];
+      wants = [ "network-online.target" "sys-subsystem-net-devices-WAN.device" "update_domain.path" ];
+      bindsTo = [ "sys-subsystem-net-devices-WAN.device" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${check-script}";
+        User = "nobody";
+        Group = "nogroup";
+      };
+    };
+
+    systemd.timers.update_domain = {
+      description = "Timer for Domain Update";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnBootSec = "1min";
+        OnUnitActiveSec = "5min";
+        Unit = "update_domain.service";
+      };
+    };
+
+    systemd.paths.update_domain = {
+      description = "Trigger Domain Update when WAN IP changes";
+      wantedBy = [ "multi-user.target" ];
+      pathConfig = {
+        PathExistsGlob = "/run/systemd/netif/leases/*";
+        Unit = "update_domain";
+      };
     };
   };
 }
