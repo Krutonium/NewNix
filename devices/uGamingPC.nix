9860f2f66c542b9067ba55e9eda8604aed2544e3
Krutonium <PFCKrutonium@gmail.com>
2025-02-24 12:36:22 -0500

    Switch to Stable nVidia

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 let
   kernel = with pkgs; unstable.linuxPackages_zen;
-  video = config.boot.kernelPackages.nvidiaPackages.beta;
+  video = config.boot.kernelPackages.nvidiaPackages.stable;
   pkgAfterFbc =
     if builtins.hasAttr video.version pkgs.nvidia-patch-list.fbc
     then pkgs.nvidia-patch.patch-fbc video
