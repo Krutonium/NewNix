4845e67c77074d07e5fd00f0afe5fa444bbb3761
Krutonium <PFCKrutonium@gmail.com>
2025-02-23 21:18:19 -0500

    Install Rust

--- user/krutonium-hm-extras/packages-desktop.nix
+++ user/krutonium-hm-extras/packages-desktop.nix
@@ -48,6 +48,9 @@ in
     (pkgs.master.jetbrains.plugins.addPlugins pkgs.master.jetbrains.idea-ultimate [ "github-copilot" "nixidea" ])
     (pkgs.master.jetbrains.plugins.addPlugins pkgs.master.jetbrains.rider [ "github-copilot" ])
     (pkgs.master.jetbrains.plugins.addPlugins pkgs.master.jetbrains.rust-rover [ "github-copilot" ])
+    pkgs.cargo 
+    pkgs.rustc
+
     pkgs.mono
     pkgs.nil
     pkgs.nixd
