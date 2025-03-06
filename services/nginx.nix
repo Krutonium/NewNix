812ecb1018d323c71235f07bf55ba8caa2683a9a
Krutonium <PFCKrutonium@gmail.com>
2025-03-06 00:20:26 -0500

    Remove Duckdns

--- services/nginx.nix
+++ services/nginx.nix
@@ -188,7 +188,7 @@ in
       };
       "gitea.krutonium.ca" = {
         # Gitea hostname
-        serverAliases = [ "krutonium.duckdns.org" "git.krutonium.ca" ];
+        serverAliases = [ "git.krutonium.ca" ];
         enableACME = true; # Use ACME certs
         forceSSL = true; # Force SSL
         locations."/".proxyPass = "http://127.0.0.1:3001/"; # Proxy Gitea
