5853cc3260d60197d3b795a061ed5645058283e0
Krutonium <PFCKrutonium@gmail.com>
2025-03-21 20:59:52 -0400

    Update Script to deal with my random ass nixos bugs

--- update_everything.sh
+++ update_everything.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-sudo nixos-rebuild switch --flake .#uGamingPC --target-host uGamingPC
-sudo nixos-rebuild switch --flake .#uMsiLaptop --target-host uMsiLaptop
-sudo nixos-rebuild switch --flake .#uServerHost --target-host uServerHost
-sudo nixos-rebuild switch --flake .#uWebServer --target-host uWebServer
+retry() {
+    local cmd="$1"
+    while true; do
+        echo "Running: $cmd"
+        eval "$cmd" && break
+        echo "Command failed. Retrying in 5 seconds..."
+        sleep 5
+    done
+}
+
+retry "sudo nixos-rebuild switch --flake .#uGamingPC --target-host uGamingPC"
+retry "sudo nixos-rebuild switch --flake .#uMsiLaptop --target-host uMsiLaptop"
+retry "sudo nixos-rebuild switch --flake .#uServerHost --target-host uServerHost"
+retry "sudo nixos-rebuild switch --flake .#uWebServer --target-host uWebServer"
+        
