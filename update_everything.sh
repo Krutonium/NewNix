46cdc6f084552a3419702101e309f321428412d4
Krutonium <PFCKrutonium@gmail.com>
2025-03-26 16:49:23 -0400

    Script + Lix Updates.

--- update_everything.sh
+++ update_everything.sh
@@ -13,3 +13,7 @@ retry "sudo nixos-rebuild switch --flake .#uGamingPC --target-host 10.2"
 retry "sudo nixos-rebuild switch --flake .#uMsiLaptop --target-host 10.4"
 retry "sudo nixos-rebuild switch --flake .#uServerHost --target-host 10.3"
 retry "sudo nixos-rebuild switch --flake .#uWebServer --target-host 10.1"
+retry 'ssh 10.1 "sudo nix-collect-garbage -d && nix-collect-garbage -d"'
+retry 'ssh 10.2 "sudo nix-collect-garbage -d && nix-collect-garbage -d"'
+retry 'ssh 10.3 "sudo nix-collect-garbage -d && nix-collect-garbage -d"'
+retry 'ssh 10.4 "sudo nix-collect-garbage -d && nix-collect-garbage -d"'
