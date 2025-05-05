ca779240c8816af7d0a40ececbdaf0ca53b7f135
Krutonium <PFCKrutonium@gmail.com>
2025-05-05 17:52:28 -0400

    Disable Powersaving to make Speakers Pleasant Again

--- audio/pipewire.nix
+++ audio/pipewire.nix
@@ -8,6 +8,22 @@ with lib;
 with builtins;
 let
   cfg = config.sys.audio;
+  
+  wp_disable_powersaving = {
+    "monitor.alsa.rules" = [{
+      matches = [{
+        "node.name" = "~alsa_input.*";
+        # Matches all input devices, according to the Arch Wiki
+      }];
+      actions = {
+        update-props = {
+          "session.suspend-timeout-seconds" = 0;
+          # Disable Session Suspend
+        };
+      };
+    }];
+  };
+  
   pw_fix_crackle = {
     "context.properties" = {
       "default.clock.rate" = 48000;
@@ -62,7 +78,10 @@ in
       pipewire = {
         enable = true;
         package = pkgs.unstable.pipewire;
-        wireplumber.enable = true;
+        wireplumber = {
+          enable = true;
+          extraConfig."99-disable-powersave" = wp_disable_powersaving;
+        };
         audio.enable = true;
         alsa = {
           enable = true;
