c1583b4cc7c9d050c759d1c4e7eb79153a1a0c51
Krutonium <PFCKrutonium@gmail.com>
2025-05-09 13:43:11 -0400

    All the changes

--- devices/uServerHost.nix
+++ devices/uServerHost.nix
@@ -205,7 +205,7 @@ in
       name = "create_chronicles";
       java = pkgs.jdk21;
       script = "";
-      enabled = false;
+      enabled = true;
       rconPort = 12348;
       rconPasswordFile = "/servers/rcon.password";
     }
