45b4dacb5b1880e713f8854052f80271c32b8617
Krutonium <PFCKrutonium@gmail.com>
2025-05-17 04:31:32 -0400

    Save Wifi Passwords

--- devices/uMsiLaptop-hw.nix
+++ devices/uMsiLaptop-hw.nix
@@ -58,6 +58,14 @@
         "subvol=home"
       ];
     };
+    "/var/lib/NetworkManager" = {
+      device = "/dev/disk/by-uuid/941617ae-329b-477d-9760-09268d5cfeef"
+      fsType = "btrfs";
+      options = [
+        "compress=zstd:15"
+        "subvol=networkmanager"
+      ];
+    };
     "/storage" = {
       device = "/dev/disk/by-uuid/3333f503-a70b-40b9-8037-8c226456bff4";
       fsType = "ext4";
