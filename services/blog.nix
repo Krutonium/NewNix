5237de81c37a92b9f1c5ba51a28d2151e49646fa
Krutonium <PFCKrutonium@gmail.com>
2025-05-29 20:14:55 -0400

    Remove Extra Stuff

--- services/blog.nix
+++ services/blog.nix
@@ -8,16 +8,9 @@ with lib;
 with builtins;
 let
   cfg = config.sys.services;
-
-  hugoTheme = builtins.fetchTarball {
-    name = "Hugo-Theme-PaperMod";
-    url = "https://github.com/adityatelange/hugo-PaperMod/archive/3e53621.tar.gz";
-    sha256 = "00hl085y8bial70jf7xnfg995qs140y96ycgmv8a9r06hsfx1zqf";
-  };
   script = pkgs.writeShellScript "blog-start" ''
     cd /home/krutonium/Blog/
     git pull
-    ln -snf ${hugoTheme} themes/PaperMod
     hugo server -D -E -b krutonium.ca -p 1313 --appendPort=false -e production
   '';
 in
