4e8e504afd50e8fb52c8e937c5a1610c7abac9a5
Krutonium <PFCKrutonium@gmail.com>
2025-06-16 15:10:36 -0400

    Make themes always match

--- user/krutonium-hm-extras/stylix.nix
+++ user/krutonium-hm-extras/stylix.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, osConfig, ... }:
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/heetch.yaml";
+    base16Scheme = osConfig.stylix.base16Scheme;
     targets = {
       gtk.flatpakSupport.enable = false;
     };
