58637121f616adcdc5c6f1f4eae639489ea06106
Krutonium <PFCKrutonium@gmail.com>
2025-07-14 22:06:12 -0400

    Change Age Key

--- common.nix
+++ common.nix
@@ -80,7 +80,7 @@ in
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile = "/home/krutonium/.ssh/age/keys.txt";
+    age.keyFile = "/home/krutonium/.ssh/id_ed25519";
     secrets.searx_secret = {
       path = "/etc/secrets/searx_secret";
     };
