f46ddb2b9dafc223950dff18400f9c1aab498ae0
Krutonium <PFCKrutonium@gmail.com>
2025-07-30 23:21:30 -0400

    Install wivrn

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -7,16 +7,8 @@
 let
   kernel = with pkgs.unstable; linuxPackages_latest;
   video = config.boot.kernelPackages.nvidiaPackages.beta;
-  pkgAfterFbc =
-    if builtins.hasAttr video.version pkgs.nvidia-patch-list.fbc then
-      pkgs.nvidia-patch.patch-fbc video
-    else
-      video;
-  finalPkg =
-    if builtins.hasAttr video.version pkgs.nvidia-patch-list.nvenc then
-      pkgs.nvidia-patch.patch-nvenc pkgAfterFbc
-    else
-      pkgAfterFbc;
+  pkgAfterFbc = if builtins.hasAttr video.version pkgs.nvidia-patch-list.fbc then pkgs.nvidia-patch.patch-fbc video else video;
+  finalPkg = if builtins.hasAttr video.version pkgs.nvidia-patch-list.nvenc then pkgs.nvidia-patch.patch-nvenc pkgAfterFbc else pkgAfterFbc;
   zenpower = config.boot.kernelPackages.zenpower;
   ddcci = config.boot.kernelPackages.ddcci-driver;
   Hostname = "uGamingPC";
@@ -267,6 +259,40 @@ in
     virtualization = {
       server = "virtd";
       windows = true;
+    };
+  };
+  services.wivrn = {
+    enable = true;
+    openFirewall = true;
+
+    # Write information to /etc/xdg/openxr/1/active_runtime.json, VR applications
+    # will automatically read this and work with WiVRn (Note: This does not currently
+    # apply for games run in Valve's Proton)
+    defaultRuntime = true;
+
+    # Run WiVRn as a systemd service on startup
+    autoStart = true;
+
+    # Config for WiVRn (https://github.com/WiVRn/WiVRn/blob/master/docs/configuration.md)
+    config = {
+      enable = true;
+      json = {
+        # 1.0x foveation scaling
+        scale = 1.0;
+        # 100 Mb/s
+        bitrate = 100000000;
+        encoders = [
+          {
+            encoder = "vaapi";
+            codec = "h265";
+            # 1.0 x 1.0 scaling
+            width = 1.0;
+            height = 1.0;
+            offset_x = 0.0;
+            offset_y = 0.0;
+          }
+        ];
+      };
     };
   };
   #programs.steam.enable = true;
