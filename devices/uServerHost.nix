e3a9c6a891965e761e718236e9ca23e4dbc5bee8
Krutonium <PFCKrutonium@gmail.com>
2025-09-09 16:26:45 -0400

    Use nVidia 580

--- devices/uServerHost.nix
+++ devices/uServerHost.nix
@@ -7,7 +7,7 @@
   ...
 }:
 let
-  kernel = with pkgs; linuxPackages;
+  kernel = with pkgs; linuxPackages_nvidia;
   btrfsDisk = "/dev/disk/by-label/WorkDisk";
   video = config.boot.kernelPackages.nvidiaPackages.stable;
   Hostname = "uServerHost";
