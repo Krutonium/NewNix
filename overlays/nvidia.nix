d83d1917d886059225fb49862097ed1339b47a8c
Krutonium <PFCKrutonium@gmail.com>
2025-09-09 16:29:29 -0400

    Persistence SHA256

--- overlays/nvidia.nix
+++ overlays/nvidia.nix
@@ -12,7 +12,7 @@ self: super: {
         sha256_64bit = "sha256-Bh5I4R/lUiMglYEdCxzqm3GLolQNYFB0/yJ/zgYoeYw=";
         openSha256 = "sha256-8/7ZrcwBMgrBtxebYtCcH5A51u3lAxXTCY00LElZz08=";
         settingsSha256 = "sha256-lx1WZHsW7eKFXvi03dAML6BoC5glEn63Tuiz3T867nY=";
-        persistencedSha256 = "";
+        persistencedSha256 = "sha256-1JCk2T3H5NNFQum0gA9cnio31jc0pGvfGIn2KkAz9kA=";
       };
 
       # Patch the driver (if patches are available)
