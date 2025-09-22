e719fafa7ce8f4c9e2bda685dc503bb52365f98b
Krutonium <PFCKrutonium@gmail.com>
2025-09-21 23:14:57 -0400

    nVidia Hashes

--- overlays/nvidia.nix
+++ overlays/nvidia.nix
@@ -9,19 +9,27 @@ self: super: {
         # Remove all hashes
         # Build repeatedly until all hashes are satisfied.
         version = "580.82.09";
-        sha256_64bit = "";
-        openSha256 = "";
-        settingsSha256 = "";
-        persistencedSha256 = "";
+        sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+        sha256_aarch64 = "sha256-6tHiAci9iDTKqKrDIjObeFdtrlEwjxOHJpHfX4GMEGQ=";
+        openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
+        settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+        persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
       };
 
       # Patch the driver (if patches are available)
       # to enable nvenc unlimited streams, and framebuffer capture
       patched =
         let
-          pkgAfterFbc = if builtins.hasAttr base.version super.pkgs.nvidia-patch-list.fbc then super.pkgs.nvidia-patch.patch-fbc base else base;
+          pkgAfterFbc =
+            if builtins.hasAttr base.version super.pkgs.nvidia-patch-list.fbc then
+              super.pkgs.nvidia-patch.patch-fbc base
+            else
+              base;
         in
-        if builtins.hasAttr base.version super.pkgs.nvidia-patch-list.nvenc then super.pkgs.nvidia-patch.patch-nvenc pkgAfterFbc else pkgAfterFbc;
+        if builtins.hasAttr base.version super.pkgs.nvidia-patch-list.nvenc then
+          super.pkgs.nvidia-patch.patch-nvenc pkgAfterFbc
+        else
+          pkgAfterFbc;
     in
     {
       # And then override the nVidia Drivers with our new driver
