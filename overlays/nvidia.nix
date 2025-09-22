1ec5c20e3917c32d4de0a9e89160226461f89b62
Krutonium <PFCKrutonium@gmail.com>
2025-09-21 23:10:29 -0400

    580.82.09 nVidia Driver

--- overlays/nvidia.nix
+++ overlays/nvidia.nix
@@ -8,11 +8,11 @@ self: super: {
         # Update version number
         # Remove all hashes
         # Build repeatedly until all hashes are satisfied.
-        version = "580.82.07";
-        sha256_64bit = "sha256-Bh5I4R/lUiMglYEdCxzqm3GLolQNYFB0/yJ/zgYoeYw=";
-        openSha256 = "sha256-8/7ZrcwBMgrBtxebYtCcH5A51u3lAxXTCY00LElZz08=";
-        settingsSha256 = "sha256-lx1WZHsW7eKFXvi03dAML6BoC5glEn63Tuiz3T867nY=";
-        persistencedSha256 = "sha256-1JCk2T3H5NNFQum0gA9cnio31jc0pGvfGIn2KkAz9kA=";
+        version = "580.82.09";
+        sha256_64bit = "";
+        openSha256 = "";
+        settingsSha256 = "";
+        persistencedSha256 = "";
       };
 
       # Patch the driver (if patches are available)
