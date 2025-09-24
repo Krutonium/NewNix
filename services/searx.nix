87e28958b95b749d87f8e8699227d181912ac19b
Krutonium <PFCKrutonium@gmail.com>
2025-09-24 17:23:32 -0400

    Remove Attempt; No Luck

--- services/searx.nix
+++ services/searx.nix
@@ -120,26 +120,6 @@ in
             weight = 10;
           };
           "google news".disabled = false;
-          "NixOS Packages" = {
-            engine = "json_engine";
-            shortcut = "nixpkgs";
-            search_url = "https://search.nixos.org/packages?channel=unstable&query={query}&format=json";
-            url = "https://search.nixos.org/packages?query={query}";
-            categories = [
-              "packages"
-              "nixos"
-            ];
-          };
-          "NixOS Options" = {
-            engine = "json_engine";
-            shortcut = "nixopts";
-            search_url = "https://search.nixos.org/options?channel=unstable&query={query}&format=json";
-            url = "https://search.nixos.org/options?query={query}";
-            categories = [
-              "options"
-              "nixos"
-            ];
-          };
         };
       };
     };
