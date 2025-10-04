35a5d64796616615c750558227a92f058067d2e8
Krutonium <PFCKrutonium@gmail.com>
2025-10-04 14:50:45 -0400

    Update nVidia Driver to 580.95.05

--- overlays/nvidia.nix
+++ overlays/nvidia.nix
@@ -2,7 +2,8 @@ self: super: {
   # Provide a helper to apply the patched NVIDIA driver to any linuxPackages set
 
   # To use: `kernel = pkgs.nvidiaFor pkgs.linuxPackages_the_version;`
-  nvidiaFor = linuxPackages:
+  nvidiaFor =
+    linuxPackages:
     linuxPackages.extend (
       selfnv: supernv:
       let
@@ -11,27 +12,21 @@ self: super: {
           # - Update version number
           # - Remove all hashes
           # - Build repeatedly until all hashes are satisfied.
-          version = "580.82.09";
-          sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
-          sha256_aarch64 = "sha256-6tHiAci9iDTKqKrDIjObeFdtrlEwjxOHJpHfX4GMEGQ=";
-          openSha256 = "sha256-YB+mQD+oEDIIDa+e8KX1/qOlQvZMNKFrI5z3CoVKUjs=";
-          settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
-          persistencedSha256 = "sha256-lbYSa97aZ+k0CISoSxOMLyyMX//Zg2Raym6BC4COipU=";
+          version = "580.95.05";
+          sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+          sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
+          openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
+          settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+          persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
         };
 
         patched =
           let
-            pkgAfterFbc =
-              if builtins.hasAttr base.version super.pkgs.nvidia-patch-list.fbc then
-                super.pkgs.nvidia-patch.patch-fbc base
-              else
-                base;
+            pkgAfterFbc = if builtins.hasAttr base.version super.pkgs.nvidia-patch-list.fbc then super.pkgs.nvidia-patch.patch-fbc base else base;
           in
-          if builtins.hasAttr base.version super.pkgs.nvidia-patch-list.nvenc then
-            super.pkgs.nvidia-patch.patch-nvenc pkgAfterFbc
-          else
-            pkgAfterFbc;
-      in {
+          if builtins.hasAttr base.version super.pkgs.nvidia-patch-list.nvenc then super.pkgs.nvidia-patch.patch-nvenc pkgAfterFbc else pkgAfterFbc;
+      in
+      {
         # Preserve mkDriver passthrough
         nvidiaPackages.mkDriver = supernv.nvidiaPackages.mkDriver;
 
