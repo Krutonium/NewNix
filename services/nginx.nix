efc5dded4bace8b603bc1c0bc5b6f07e354697a6
Krutonium <PFCKrutonium@gmail.com>
2025-11-04 17:34:10 -0500

    Add Temporary Publish URL

--- services/nginx.nix
+++ services/nginx.nix
@@ -68,7 +68,7 @@ in
              hls_playlist_length 60s;
              hls_nested on;
 
-             # on_publish http://127.0.0.1:8081/auth;
+             on_publish http://10.0.0.2:8081;
            }
           }
         }
