53cff55259ade7cafc4671f234f9a77bce02c2eb
Krutonium <PFCKrutonium@gmail.com>
2025-11-20 17:30:42 -0500

    Fix SDDM Login Layout Automatically

--- desktop/sddm.nix
+++ desktop/sddm.nix
@@ -19,5 +19,17 @@ in
         };
       };
     };
+    systemd.tmpfiles.rules =
+      let
+        username = "krutonium";
+      in
+      [
+        # Ensure SDDM .config directory exists
+        "d /var/lib/sddm/.config 0755 sddm sddm -"
+
+        # Copy KWin output config into SDDM user’s config
+        # C = copy only if missing (use C! if you want forced overwrite)
+        "C! /var/lib/sddm/.config/kwinoutputconfig.json 0600 sddm sddm - /home/${username}/.config/kwinoutputconfig.json"
+      ];
   };
 }
