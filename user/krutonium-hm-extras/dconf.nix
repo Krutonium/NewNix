47ec4626cdb39850c52f0777e1c32fe19be09751
Krutonium <PFCKrutonium@gmail.com>
2025-12-19 02:02:01 -0500

    Remove dmabuf

--- user/krutonium-hm-extras/dconf.nix
+++ user/krutonium-hm-extras/dconf.nix
@@ -125,7 +125,7 @@ lib.mkIf isGnome {
       focus-change-on-pointer-rest = true;
       overlay-key = "Super_L";
       workspaces-only-on-primary = true;
-      experimental-features = [ "variable-refresh-rate" "dma-buf-screen-sharing" ];
+      experimental-features = [ "variable-refresh-rate" ];
     };
 
     "com/github/amezin/ddterm" = {
