1b4b585a2ea576e7b061f6f4d614ddfb09e0c788
Krutonium <PFCKrutonium@gmail.com>
2026-01-27 11:46:55 -0500

    Ozone WL

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -239,6 +239,7 @@ in
   #environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables = {
     #__EGL_VENDOR_LIBRARY_FILENAMES="${config.hardware.nvidia.package}/share/glvnd/egl_vendor.d/10_nvidia.json";
+    NIXOS_OZONE_WL="1";
   };
   sys = {
     boot = {
