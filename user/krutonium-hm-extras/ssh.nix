19274b342dcd251a1206cc348b7fefeba306992c
Krutonium <PFCKrutonium@gmail.com>
2026-04-03 19:28:19 -0400

    Remove Nulling of SSH target known_hosts; add script to update known_hosts

--- user/krutonium-hm-extras/ssh.nix
+++ user/krutonium-hm-extras/ssh.nix
@@ -12,32 +12,26 @@
         hostname = "10.9";
         user = "deck";
         compression = true;
-        userKnownHostsFile = "/dev/null";
       };
       "uWebServer" = {
         hostname = "10.1";
         compression = true;
-        userKnownHostsFile = "/dev/null";
       };
       "uGamingPC" = {
         hostname = "10.2";
         compression = true;
-        userKnownHostsFile = "/dev/null";
       };
       "uRenderPC" = {
         hostname = "10.3";
         compression = true;
-        userKnownHostsFile = "/dev/null";
       };
       "darth" = {
         hostname = "195.35.113.15";
         compression = true;
-        userKnownHostsFile = "/dev/null";
       };
       "uServerHost" = {
         hostname = "10.0.0.3";
         compression = true;
-        userKnownHostsFile = "/dev/null";
       };
     };
     extraConfig = ''
