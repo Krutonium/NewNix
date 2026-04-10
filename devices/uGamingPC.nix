317efb8663d54bcdf6a211ca36ff4cc3582e75ef
Krutonium <PFCKrutonium@gmail.com>
2026-04-10 02:44:11 -0400

    WiVRn

--- devices/uGamingPC.nix
+++ devices/uGamingPC.nix
@@ -230,10 +230,6 @@ in
     enable = false;
     openFirewall = true;
     package = pkgs.unstable.wivrn;
-    # Write information to /etc/xdg/openxr/1/active_runtime.json, VR applications
-    # will automatically read this and work with WiVRn (Note: This does not currently
-    # apply for games run in Valve's Proton)
-    defaultRuntime = true;
 
     # Run WiVRn as a systemd service on startup
     autoStart = true;
