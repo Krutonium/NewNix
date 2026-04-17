bde81546ce3dbcd2d73026a09e2d550a0a4a0ee8
Krutonium <PFCKrutonium@gmail.com>
2026-04-17 17:12:05 -0400

    Switch Interface

--- services/torrent.nix
+++ services/torrent.nix
@@ -57,8 +57,8 @@ in
         max_active_seeding = "500";
         max_active_downloading = "500";
         max_active_limit = "1000";
-        listen_interface = "WAN";
-        outgoing_interface = "WAN";
+        listen_interface = "br0";
+        outgoing_interface = "br0";
         outgoing_ports = [
           50024
           50050
