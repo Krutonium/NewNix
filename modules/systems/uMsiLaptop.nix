c4c823aea4cb6d6ddcd4d4b42d9a009b68d9dea1
Krutonium <PFCKrutonium@gmail.com>
2026-05-07 00:23:42 -0400

    nVidia!

--- modules/systems/uMsiLaptop.nix
+++ modules/systems/uMsiLaptop.nix
@@ -101,7 +101,7 @@
         powerManagement.finegrained = true; # Turn GPU off when not in use (requires modesetting)
         open = false; # 950M requires proprietary driver
         nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
         prime = {
           offload = {
             enable = true;
