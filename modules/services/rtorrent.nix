1a902384ac9e41d451e8cb8cf1685ca98f31111a
Krutonium <PFCKrutonium@gmail.com>
2026-05-12 04:30:06 -0400

    Eh

--- modules/services/rtorrent.nix
+++ modules/services/rtorrent.nix
@@ -4,7 +4,7 @@
     { config, pkgs, ... }:
     let
       peerPort = 51412;
-      web-port = "8112";
+      web-port = 8112;
     in
     {
       services = {
@@ -22,7 +22,7 @@
           "flood.${config.networking.domain}" = {
             forceSSL = true;
             enableACME = true;
-            locations."/".proxyPass = "http://127.0.0.1:${web-port}";
+            locations."/".proxyPass = "http://127.0.0.1:${toString web-port}";
             locations."/robots.txt" = {
               extraConfig = ''
                 rewrite ^/(.*)  $1;
