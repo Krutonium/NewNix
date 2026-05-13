86d5e01fa7d04e79c32845191b591b257d526a67
Krutonium <PFCKrutonium@gmail.com>
2026-05-13 14:23:46 -0400

    Maybe rTorrent

--- modules/services/rtorrent.nix
+++ modules/services/rtorrent.nix
@@ -7,36 +7,54 @@
       web-port = 8112;
     in
     {
+      sops.secrets.basicAuth = {
+        owner = "nginx";
+        group = "nginx";
+        mode = "0440";
+      };
       services = {
         rtorrent = {
           enable = true;
           openFirewall = true;
           port = peerPort;
           dataPermissions = "0755";
-          dataDir = "/media2/downloads/";
+          dataDir = "/media2/rTorrent/data";
+          downloadDir = "/media2/rTorrent/downloads/";
           configText = ''
             dht.mode.set = disable
             protocol.pex.set = no
             trackers.use_udp.set = no
             protocol.encryption.set = allow_incoming,try_outgoing,enable_retry
+            network.scgi.open_port = 127.0.0.1:5000
+            throttle.max_uploads.set = 0
+            throttle.max_uploads.global.set = 250
+            throttle.min_peers.normal.set = 20
+            throttle.max_peers.normal.set = 60
+            throttle.min_peers.seed.set = 30
+            throttle.max_peers.seed.set = 80
+            trackers.numwant.set = 80
           '';
 
         }; # /run/rtorrent/rpc.sock
         flood = {
           enable = true;
           port = web-port;
+          extraArgs = [
+            "--rtorrent"
+            "--rtsocket"
+            "127.0.0.1:5000"
+          ];
         };
         nginx.virtualHosts = {
           "flood.${config.networking.domain}" = {
             forceSSL = true;
             enableACME = true;
             locations."/".proxyPass = "http://127.0.0.1:${toString web-port}";
+            basicAuthFile = config.sops.secrets.basicAuth.path;
             locations."/robots.txt" = {
               extraConfig = ''
                 rewrite ^/(.*)  $1;
                 return 200 "User-agent: *\nDisallow: /";
-                auth_basic "Restricted Access";
-                auth_basic_user_file /persist/httpAuth;
               '';
             };
           };
