20f6335bd518320c8de800b7446f9be49b15d9d5
Krutonium <PFCKrutonium@gmail.com>
2026-05-22 17:44:05 -0400

    Attic should work now

--- modules/services/attic.nix
+++ modules/services/attic.nix
@@ -2,13 +2,28 @@
 {
   flake.nixosModules.attic =
     { config, ... }:
+    let
+      user = "atticd";
+    in
     {
+      users.users.${user} = {
+        isSystemUser = true;
+        group = "atticd";
+      };
+      users.groups.atticd = {};
+      sops.secrets.atticsecret = {
+        owner = user;
+      };
       services.atticd = {
         enable = true;
-        credentialsFile = config.sops.secrets."attic-secret".path;
+        environmentFile = config.sops.secrets.atticsecret.path;
+        user = user;
         settings = {
           listen = "127.0.0.1:8080";
-          allowed-hosts = [ "cache.krutonium.ca" "10.0.0.3" ];
+          allowed-hosts = [
+            "cache.krutonium.ca"
+            "10.0.0.3"
+          ];
           api-endpoint = "https://cache.krutonium.ca/";
           storage = {
             type = "local";
