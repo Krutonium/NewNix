3f762e4fdb0be08513bc68a738de1197bfec5bb4
Krutonium <PFCKrutonium@gmail.com>
2026-05-21 23:31:18 -0400

    ollama version of cuda override

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -12,6 +12,18 @@
           port = 11434;
           openFirewall = true;
           acceleration = "cuda";
+          package = pkgs.unstable.ollama-cuda.overrideAttrs (
+            final: prev: {
+              preBuild = ''
+                cmake -B build \
+                  -DCMAKE_SKIP_BUILD_RPATH=ON \
+                  -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
+                  -DCMAKE_CUDA_ARCHITECTURES='61' \
+
+                cmake --build build -j $NIX_BUILD_CORES
+              '';
+            }
+          );
         };
         nextjs-ollama-llm-ui = {
           enable = true;
