2f8a1ebcccf2f82f1dd2d36c73666cd88f86af23
Krutonium <PFCKrutonium@gmail.com>
2026-05-27 18:19:54 -0400

    Fixup SSH Auto

--- modules/services/hostKeys.nix
+++ modules/services/hostKeys.nix
@@ -8,36 +8,65 @@
         uGamingPC = {
           publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKoAMeIuHglCBVUSiSPqx1BsNq7sQZ7Y1bRG2y26OgLl";
           sopsSecret = "ssh_host_key_uGamingPC";
-          aliases = {};
+          aliases = [
+            "10.0.0.2"
+            "fd00:beef::2"
+          ];
         };
         uWebServer = {
           publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJnrA9o97fSY1vHxOconNP1ZQpwZRD0sKQSTjhxIfJt2";
           sopsSecret = "ssh_host_key_uWebserver";
-          aliases = {};
+          aliases = [
+            "10.0.0.1"
+            "fd00:beef::1"
+            "git.krutonium.ca"
+            "krutonium.ca"
+          ];
         };
         uMsiLaptop = {
           publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBVKSuf1To5jXsPl4RMwY1aGAbhP5c6gGShmWjG6apnM";
           sopsSecret = "ssh_host_key_uMsiLaptop";
-          aliases = {};
+          aliases = [
+            "10.0.0.5"
+            "fd00:beef::5"
+          ];
         };
         uServerHost = {
           publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINYSSM1N25CKH8HIEsl49Vrilimzc1C7/S5oZcR37cjY";
           sopsSecret = "ssh_host_key_uServerHost";
-          aliases = {};
+          aliases = [
+            "10.0.0.3"
+            "fd00:beef::3"
+          ];
         };
       };
 
       thisHost = config.networking.hostName;
       thisCfg = hosts.${thisHost} or null;
 
-      allAliases = lib.foldlAttrs (acc: _name: cfg:
-        acc // lib.mapAttrs (alias: address: { inherit address; publicKey = cfg.publicKey; }) (cfg.aliases or {})
-      ) {} hosts;
-    in {
-      programs.ssh.knownHosts = lib.mapAttrs (name: cfg: {
-        hostNames = [ name ];
-        publicKey = cfg.publicKey;
-      }) hosts;
+      allAliases = lib.foldlAttrs (
+        acc: _name: cfg:
+        acc
+        // lib.mapAttrs (alias: address: {
+          inherit address;
+          publicKey = cfg.publicKey;
+        }) (cfg.aliases or { })
+      ) { } hosts;
+    in
+    {
+      programs.ssh.knownHosts = lib.foldlAttrs (
+        acc: name: cfg:
+        acc
+        // lib.listToAttrs (
+          map (alias: {
+            name = alias;
+            value = {
+              hostNames = [ alias ];
+              publicKey = cfg.publicKey;
+            };
+          }) ([ name ] ++ (cfg.aliases or [ ]))
+        )
+      ) { } hosts;
 
       sops.secrets = lib.optionalAttrs (thisCfg != null) {
         ${thisCfg.sopsSecret} = {
@@ -47,9 +76,11 @@
           mode = "0600";
         };
       };
-      services.openssh.hostKeys = lib.optionals (thisCfg != null) [{
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }];
+      services.openssh.hostKeys = lib.optionals (thisCfg != null) [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
     };
 }
