22ca15f7b807d7a982a46f1561b01c7de7dfb1f0
Krutonium <PFCKrutonium@gmail.com>
2026-06-12 17:44:53 -0400

    Fix Ollama

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -13,14 +13,9 @@
           openFirewall = true;
           package = pkgs.unstable.ollama-cuda.overrideAttrs (
             final: prev: {
-              preBuild = ''
-                cmake -B build \
-                  -DCMAKE_SKIP_BUILD_RPATH=ON \
-                  -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
-                  -DCMAKE_CUDA_ARCHITECTURES='61' \
-
-                cmake --build build -j $NIX_BUILD_CORES
-              '';
+              cmakeFlags = (prev.cmakeFlags or []) ++ [
+                "-DCMAKE_CUDA_ARCHITECTURE=61"
+              ];
             }
           );
         };
