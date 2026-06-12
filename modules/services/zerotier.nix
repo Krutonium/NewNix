312fc2489d4dec218497bee432ed59af6b5ef77e
Krutonium <PFCKrutonium@gmail.com>
2026-06-12 05:49:46 -0400

    Rename Interface

--- modules/services/zerotier.nix
+++ modules/services/zerotier.nix
@@ -7,5 +7,9 @@
         enable = true;
         joinNetworks = [ "b103a835d233ec24" ];
       };
+      systemd.network.links."10-zerotier" = {
+        matchConfig.OriginalName = "ztqtizxwpw";
+        linkConfig.Name = "zt0";
+      };
     };
 }
