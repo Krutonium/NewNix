3c585709b7c7d8d782144d5dde38a1e4c6f4709e
Krutonium <PFCKrutonium@gmail.com>
2026-06-25 19:16:04 -0400

    Fix and Format VSCode nix file

--- modules/users/krutonium-hm/vscode.nix
+++ modules/users/krutonium-hm/vscode.nix
@@ -1,7 +1,12 @@
 { self, ... }:
 {
   flake.homeModules.vscode =
-    { pkgs, ... }:
+    {
+      pkgs,
+      lib,
+      osConfig,
+      ...
+    }:
     let
       dotnet = (
         with pkgs.dotnetCorePackages;
@@ -13,77 +18,80 @@
       );
     in
     {
-      home.sessionVariables = {
-        DOTNET_ROOT = "${dotnet}/share/dotnet";
-      };
-      programs.vscode = {
-        enable = true;
-        package = pkgs.vscode;
+      config = lib.mkIf (osConfig.services.displayManager.gdm.enable == true) {
 
-        extensions = with pkgs.vscode-extensions; [
-          # Nix
-          jnoortheen.nix-ide
-          mkhl.direnv
+        home.sessionVariables = {
+          DOTNET_ROOT = "${dotnet}/share/dotnet";
+        };
+        programs.vscode = {
+          enable = true;
+          package = pkgs.vscode;
 
-          # C#
-          ms-dotnettools.csharp
+          extensions = with pkgs.vscode-extensions; [
+            # Nix
+            jnoortheen.nix-ide
+            mkhl.direnv
 
-          # Quality of life
-          editorconfig.editorconfig
+            # C#
+            ms-dotnettools.csharp
 
-          # Visual Studio Keybindings
-          (pkgs.vscode-utils.extensionFromVscodeMarketplace {
-            publisher = "ms-vscode";
-            name = "vs-keybindings";
-            version = "0.2.1";
-            sha256 = "sha256-NnLjx3fKldg6DSA4ssUt0Vevm1w8KnjEZTINZxqM7cA=";
-          })
-        ];
+            # Quality of life
+            editorconfig.editorconfig
 
-        userSettings = {
-          # Nix IDE — use nixd as the language server
-          "nix.enableLanguageServer" = true;
-          "nix.serverPath" = "nixd";
-          "nix.serverSettings" = {
-            nixd = {
-              formatting = {
-                command = [ "nixfmt" ];
-              };
-              options = {
-                # Point nixd at your flake so it can resolve options
-                nixos = {
-                  expr = "(builtins.getFlake \"${self}\").nixosConfigurations.uGamingPC.options";
+            # Visual Studio Keybindings
+            (pkgs.vscode-utils.extensionFromVscodeMarketplace {
+              publisher = "ms-vscode";
+              name = "vs-keybindings";
+              version = "0.2.1";
+              sha256 = "sha256-NnLjx3fKldg6DSA4ssUt0Vevm1w8KnjEZTINZxqM7cA=";
+            })
+          ];
+
+          userSettings = {
+            # Nix IDE — use nixd as the language server
+            "nix.enableLanguageServer" = true;
+            "nix.serverPath" = "nixd";
+            "nix.serverSettings" = {
+              nixd = {
+                formatting = {
+                  command = [ "nixfmt" ];
                 };
-                home_manager = {
-                  expr = "(builtins.getFlake \"${self}\").homeConfigurations.krutonium.options";
+                options = {
+                  # Point nixd at your flake so it can resolve options
+                  nixos = {
+                    expr = "(builtins.getFlake \"${self}\").nixosConfigurations.uGamingPC.options";
+                  };
+                  home_manager = {
+                    expr = "(builtins.getFlake \"${self}\").homeConfigurations.krutonium.options";
+                  };
                 };
               };
             };
+
+            # C# / OmniSharp
+            "dotnet.defaultSolution" = "disable";
+            "omnisharp.enableRoslynAnalyzers" = true;
+            "omnisharp.enableEditorConfigSupport" = true;
+            "omnisharp.organizeImportsOnFormat" = true;
+
+            # Editor
+            "editor.formatOnSave" = true;
+            "editor.rulers" = [ 120 ];
+            "editor.bracketPairColorization.enabled" = true;
+            "editor.guides.bracketPairs" = "active";
+            "editor.inlayHints.enabled" = "on";
+            "files.trimTrailingWhitespace" = true;
+            "files.insertFinalNewline" = true;
+
+            # Telemetry off
+            "telemetry.telemetryLevel" = "off";
           };
-
-          # C# / OmniSharp
-          "dotnet.defaultSolution" = "disable";
-          "omnisharp.enableRoslynAnalyzers" = true;
-          "omnisharp.enableEditorConfigSupport" = true;
-          "omnisharp.organizeImportsOnFormat" = true;
-
-          # Editor
-          "editor.formatOnSave" = true;
-          "editor.rulers" = [ 120 ];
-          "editor.bracketPairColorization.enabled" = true;
-          "editor.guides.bracketPairs" = "active";
-          "editor.inlayHints.enabled" = "on";
-          "files.trimTrailingWhitespace" = true;
-          "files.insertFinalNewline" = true;
-
-          # Telemetry off
-          "telemetry.telemetryLevel" = "off";
         };
+        home.packages = with pkgs; [
+          nixd
+          dotnet
+          nixfmt
+        ];
       };
-      home.packages = with pkgs; [
-        nixd
-        dotnet
-        nixfmt
-      ];
     };
 }
