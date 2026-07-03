c51da2b0de819c4928b96bb971819889c4833a3a
Krutonium <PFCKrutonium@gmail.com>
2026-07-03 02:47:46 -0400

    LTS Kernel

--- modules/systems/uServerHost.nix
+++ modules/systems/uServerHost.nix
@@ -24,7 +24,7 @@
   flake.nixosModules.uServerHostModule =
     { pkgs, config, ... }:
     let
-      kernel = pkgs.linuxPackages;
+      kernel = pkgs.linuxPackages_lts;
       btrfsDisk = "/dev/disk/by-label/WorkDisk";
       Hostname = "uServerHost";
     in
