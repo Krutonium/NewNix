3007ff7efcf74ad49e4a42ff95ad3d252ca32623
Krutonium <PFCKrutonium@gmail.com>
2026-07-03 02:47:10 -0400

    Switch to LTS kernel... :(

--- modules/systems/uMsiLaptop.nix
+++ modules/systems/uMsiLaptop.nix
@@ -43,7 +43,7 @@
         "rtsx_pci_sdmmc"
       ];
       #kernel = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto;
-      kernel = pkgs.linuxPackages_zen;
+      kernel = pkgs.linuxPackages_lts;
       kernelParams = [
         "nouveau.config=NvClkMode=15" # Not using Nouveau ATM. Maybe someday?
         "mitigations=off"
