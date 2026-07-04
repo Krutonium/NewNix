98584457f75ba76ac749ded102bdbb4da7e6a748
Krutonium <PFCKrutonium@gmail.com>
2026-07-04 17:36:59 -0400

    OBS-Relay: Tune Twitch Encode using Extra Compute

--- modules/services/obs-relay.nix
+++ modules/services/obs-relay.nix
@@ -14,26 +14,56 @@
         ExecStart = pkgs.writeShellScript "obs-relay-start" ''
           TWITCH_KEY=$(cat ${config.sops.secrets.twitch-stream-key.path})
           YOUTUBE_KEY=$(cat ${config.sops.secrets.youtube-stream-key.path})
-          exec ${pkgs.ffmpeg-full}/bin/ffmpeg -hwaccel cuda -hwaccel_output_format cuda \
+          exec ${pkgs.ffmpeg-full}/bin/ffmpeg \
+            -hwaccel cuda \
+            -hwaccel_output_format cuda \
             -i "srt://0.0.0.0:9999?mode=listener" \
             -filter_complex "[0:v]split=2[v1][v2]" \
-            -map "[v1]" -map 0:a \
-              -c:v h264_nvenc -preset p5 -rc cbr -b:v 6000k -maxrate 6000k -bufsize 12000k -g 120 -bf 2 \
-              -c:a aac -b:a 160k -f flv "rtmp://live.twitch.tv/app/$TWITCH_KEY" \
-            -map "[v2]" -map 0:a \
-              -c:v hevc_nvenc -preset p5 -rc cbr -b:v 20000k -maxrate 20000k -bufsize 40000k \
-              -g 120 -profile:v main \
-              -c:a aac -b:a 192k \
-              -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY"
-            -map 0:v -map 0:a -c copy \
-              -f rtsp -rtsp_flags listen "rtsp://0.0.0.0:8554/mezzanine"
-
-#            -map "[v2]" -map 0:a \
-#              -c:v h264_nvenc -preset p5 -rc cbr -b:v 20000k -maxrate 20000k -bufsize 40000k -g 120 -bf 2 \
-#              -c:a aac -b:a 192k -f flv "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY"
+            -map "[v1]" \
+            -map 0:a \
+            -c:v h264_nvenc \
+            -preset p6 \
+            -tune hq \
+            -rc cbr \
+            -multipass fullres \
+            -b:v 6000k \
+            -maxrate 6000k \
+            -bufsize 12000k \
+            -rc-lookahead 32 \
+            -spatial_aq 1 \
+            -temporal_aq 1 \
+            -aq-strength 8 \
+            -bf 3 \
+            -b_ref_mode middle \
+            -g 120 \
+            -c:a aac \
+            -b:a 160k \
+            -f flv \
+            "rtmp://live.twitch.tv/app/$TWITCH_KEY" \
+            -map "[v2]" \
+            -map 0:a \
+            -c:v hevc_nvenc \
+            -preset p5 \
+            -rc cbr \
+            -b:v 20000k \
+            -maxrate 20000k \
+            -bufsize 40000k \
+            -g 120 \
+            -profile:v main \
+            -c:a aac \
+            -b:a 192k \
+            -f flv \
+            "rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY" \
+            -map 0:v \
+            -map 0:a \
+            -c copy \
+            -f rtsp \
+            -rtsp_flags listen \
+            "rtsp://0.0.0.0:8554/mezzanine"
         '';
       };
     };
+
     networking.firewall.allowedUDPPorts = [ 9999 ];
     networking.firewall.allowedTCPPorts = [ 8554 ];
   };
