3dcce18d6b197f25f93c9a6359a704a9a89e11c9
Krutonium <PFCKrutonium@gmail.com>
2026-07-26 22:30:27 -0400

    Update Telegram & Remove Save Restrictions

--- enabled/users/krutonium-hm/packages-desktop.nix
+++ enabled/users/krutonium-hm/packages-desktop.nix
@@ -27,11 +27,11 @@
           src = pkgs.fetchFromGitHub {
             owner = "telegramdesktop";
             repo = "tdesktop";
-            rev = "ae7ab838f450b73b30ade03a87cfdb6ff4b68bd3";
-            hash = "sha256-nN6a/TPMGmB59HS7uYvNuLVAXjn2XbJWuAqlPft0jww=";
+            rev = "v7.0.2";
+            hash = "sha256-G/A5J2m1sXHD50zDmMD9ehnorAGRjnQ+YGMv6DEiJcQ=";
             fetchSubmodules = true;
           };
-          unwrapped = pkgs.telegram-desktop.unwrapped.overrideAttrs (oldAttrs: {
+          unwrapped = pkgs.unstable.telegram-desktop.unwrapped.overrideAttrs (oldAttrs: {
             inherit version src;
             patches = (oldAttrs.patches or [ ]) ++ [
               ./patches/telegram/0001-Disable-advertisements.patch
