67d0a148cef520e20b72a0d797f9d709317fca81
Krutonium <PFCKrutonium@gmail.com>
2026-07-28 14:58:35 -0400

    Enable Signing Commits

--- enabled/users/krutonium-hm/git.nix
+++ enabled/users/krutonium-hm/git.nix
@@ -9,12 +9,19 @@
           user = {
             name = "Krutonium";
             email = "PFCKrutonium@gmail.com";
+            signingKey = "~/.ssh/id_ed25519.pub";
           };
           init = {
             defaultBranch = "main";
           };
           pull = {
             rebase = true;
+          };
+          gpg = {
+            format = "ssh";
+          };
+          commit = {
+            gpgSign = true;
           };
         };
         lfs.enable = true;
