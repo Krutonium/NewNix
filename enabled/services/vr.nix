204456daf5103d5e1e809cf98f81c2c2e09dd52e
Krutonium <PFCKrutonium@gmail.com>
2026-09-03 10:39:07 -0400

    Wayvr

--- enabled/services/vr.nix
+++ enabled/services/vr.nix
@@ -7,6 +7,18 @@
         enable = true;
         openFirewall = true;
         autoStart = true;
-        package = (pkgs.wivrn.override { cudaSupport = true; });      };
+        package = (pkgs.wivrn.override { cudaSupport = true; });
+      };
+      environment.systemPackages = [ pkgs.wayvr ];
+      systemd.user.services.wayvr = {
+        description = "WayVR desktop overlay for OpenXR/OpenVR";
+        partOf = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        wantedBy = [ "graphical-session.target" ];
+        serviceConfig = {
+          ExecStart = "${pkgs.wayvr}/bin/wayvr --replace";
+          Restart = "on-failure";
+        };
+      };
     };
 }
