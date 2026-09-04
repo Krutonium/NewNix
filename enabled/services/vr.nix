82198aa525ccbd2bcbb3538856e778d8d58d5040
Krutonium <PFCKrutonium@gmail.com>
2026-09-04 01:26:07 -0400

    VR

--- enabled/services/vr.nix
+++ enabled/services/vr.nix
@@ -10,6 +10,14 @@
         package = (pkgs.wivrn.override { cudaSupport = true; });
       };
       environment.systemPackages = [ pkgs.wayvr ];
+      programs.steam.package = pkgs.steam.override {
+        extraProfile = ''
+          # Allows Monado/WiVRn to be used
+          export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
+          # Fixes timezones on VRChat
+          unset TZ
+        '';
+      };
       systemd.user.services.wayvr = {
         description = "WayVR desktop overlay for OpenXR/OpenVR";
         partOf = [ "graphical-session.target" ];
